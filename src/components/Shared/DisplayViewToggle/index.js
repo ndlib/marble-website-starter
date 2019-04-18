a77ae28b25dedb767059204bed50195400d21895
Dan Wolfe <dwolfe2@nd.edu>
2019-04-18 13:18:43 -0400

    Toggle View With Hooks

--- src/components/Shared/DisplayViewToggle/index.js
+++ src/components/Shared/DisplayViewToggle/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import ToggleButton from './ToggleButton'
+import style from './style.module.css'
+
+import listIcon from 'assets/icons/svg/baseline-view_list-24px.svg'
+import gridIcon from 'assets/icons/svg/baseline-view_module-24px.svg'
 
 export const DisplayViewToggle = ({ children }) => {
+  const [activeStyle, setActiveStyle] = useState('grid')
+
+  useEffect(() => {})
   return (
-    <div classame='placeholder-for-toggle-class'>
-      <div>Display View Toggle Buttons</div>
+    <div classame={activeStyle}>
+      <div className={style.displayViewToggleGroup}>
+        <ToggleButton
+          icon={listIcon}
+          option='list'
+          action={() => {
+            setActiveStyle('list')
+          }}
+          active={activeStyle === 'list'}
+        />
+        <ToggleButton
+          icon={gridIcon}
+          option='grid'
+          action={() => {
+            setActiveStyle('grid')
+          }}
+          active={activeStyle === 'grid'}
+        />
+      </div>
+      <br className='clearfix' />
       {children}
     </div>
   )
