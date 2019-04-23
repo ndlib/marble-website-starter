f1efd19dae7fc5644553ffae48b41323a899f7f1
Dan Wolfe <dwolfe2@nd.edu>
2019-04-23 12:38:53 -0400

    remove unnecessary props

--- src/components/APIViews/Search/SearchDisplay/index.js
+++ src/components/APIViews/Search/SearchDisplay/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import DisplayViewToggle from 'components/Shared/DisplayViewToggle'
 import PerPage from './PerPage'
 import PageNum from './PageNum'
 import Results from './Results'
 
-export const SearchDisplay = ({ searchReducer }) => {
+export const SearchDisplay = () => {
   return (
     <DisplayViewToggle>
       <PerPage />
@@ -16,12 +14,4 @@ export const SearchDisplay = ({ searchReducer }) => {
   )
 }
 
-SearchDisplay.propTypes = {
-  searchReducer: PropTypes.object.isRequired,
-}
-
-const mapStateToProps = (state) => {
-  return { ...state }
-}
-
-export default connect(mapStateToProps)(SearchDisplay)
+export default SearchDisplay
