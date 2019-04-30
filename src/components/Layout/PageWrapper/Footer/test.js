d222fa1c47d2bca06a4ec18dacd45ff52354d059
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-04-30 10:53:52 -0400

    Add footer text

--- src/components/Layout/PageWrapper/Footer/test.js
+++ src/components/Layout/PageWrapper/Footer/test.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { Footer } from './'
-import ReactMarkdown from 'react-markdown'
-import Navigation from '../../../Shared/Navigation'
+import Navigation from 'components/Shared/Navigation'
 
 test('Footer renders some divs with markdown text', () => {
-  const data = { site: { siteMetadata: {
-    menus: { footer: [ 'links go here!' ] },
-    footerText: 'footer!!',
-  } } }
+  const data = {
+    site: {
+      siteMetadata: {
+        menus: { footer: [ 'links go here!' ] },
+        footerText: '<p>footer!!</p>',
+      }
+    }
+  }
   const wrapper = shallow(<Footer data={data} />)
 
   expect(wrapper.find('footer.pageFooter').exists()).toBeTruthy()
   expect(wrapper.find('.footerInner').exists()).toBeTruthy()
   expect(wrapper.find('.footerText').exists()).toBeTruthy()
+  expect(wrapper.find('.footerText').html()).toEqual('<div class="footerText"><p>footer!!</p></div>')
   expect(wrapper.find('.footerCenter').exists()).toBeTruthy()
   expect(wrapper.find('.footerLinks').exists()).toBeTruthy()
-  expect(wrapper.find(ReactMarkdown).props().source).toEqual('footer!!')
   expect(wrapper.find(Navigation).props().links).toEqual([ 'links go here!' ])
 })
