9592530b4194022aebc36318c9f22daca06ceb76
Dan Wolfe <dwolfe2@nd.edu>
2019-05-03 13:46:39 -0400

    Update test.js

--- src/components/Shared/Image/test.js
+++ src/components/Shared/Image/test.js
@@ -5,18 +5,23 @@ import noImage from 'assets/images/noImage.svg'
 
 describe('Image', () => {
   test('src', () => {
-    const wrapper = shallow(<Image src='myImage.png' />)
+    const wrapper = shallow(<Image src='myImage.png' alt='alt Txt' />)
     expect(wrapper.find('picture').exists()).toBeTruthy()
+    expect(wrapper.find('img').props().alt).toEqual('alt Txt')
     expect(wrapper.find('img').props().src).toEqual('myImage.png')
   })
+
   test('service', () => {
-    const wrapper = shallow(<Image service='service' />)
+    const wrapper = shallow(<Image service='service' alt='alt Txt' />)
     expect(wrapper.find('picture').exists()).toBeTruthy()
+    expect(wrapper.find('img').props().alt).toEqual('alt Txt')
     expect(wrapper.find('img').props().src).toEqual('service/full/500,/0/default.jpg')
   })
+
   test('null', () => {
-    const wrapper = shallow(<Image />)
+    const wrapper = shallow(<Image alt='alt Txt' />)
     expect(wrapper.find('picture').exists()).toBeTruthy()
+    expect(wrapper.find('img').props().alt).toEqual('alt Txt')
     expect(wrapper.find('img').props().src).toEqual(noImage)
   })
 })
