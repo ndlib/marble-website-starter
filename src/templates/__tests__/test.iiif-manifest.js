d0df7811de2026560ec0fe4dcb7cb3be86e8e0b1
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-05-03 14:02:04 -0400

    Add location test

--- src/templates/__tests__/test.iiif-manifest.js
+++ src/templates/__tests__/test.iiif-manifest.js
@@ -6,8 +6,11 @@ test('it renders the manifest template', () => {
   const data = {
     iiifManifest: { id: 'manifest' },
   }
+  const location = { object: 'object' }
 
-  const wrapper = shallow(<ItemTemplate data={data} />)
+
+  const wrapper = shallow(<ItemTemplate data={data} location={location}/>)
   expect(wrapper.find('Item').exists()).toBeTruthy()
   expect(wrapper.find('Item').prop('iiifManifest')).toEqual({ id: 'manifest' })
+  expect(wrapper.find('Item').prop('location')).toEqual({ object: 'object' })  
 })
