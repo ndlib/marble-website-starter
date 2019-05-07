b11bde130b4d08f24584f58a8016135e3b5983f3
Dan Wolfe <dwolfe2@nd.edu>
2019-05-07 10:34:02 -0400

    fix paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Layout from '../components/Layout'
-import SEO from '../components/Seo'
-import Home from '../components/Home'
+import Layout from 'components/Layout'
+import SEO from 'components/Shared/Seo'
+import Home from 'components/Home'
 const IndexPage = () => (
   <Layout>
     <SEO title='Marble Digital Collections' />
