2296c2af3eb82f4bc34d02bb6e642733af7f7aba
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-05-13 13:57:50 -0400

    Hound

--- src/components/Shared/PrivateRoute/test.js
+++ src/components/Shared/PrivateRoute/test.js
@@ -5,7 +5,7 @@ import * as Gatsby from 'gatsby'
 import * as auth from 'utils/auth'
 
 test('it renders the page if we are not testing login', () => {
-  const wrapper = shallow(<PrivateRoute location={ {} } testLogin={false}>TEXT</PrivateRoute>)
+  const wrapper = shallow(<PrivateRoute location={{}} testLogin={false}>TEXT</PrivateRoute>)
 
   expect(wrapper.text()).toEqual('TEXT')
 })
@@ -13,7 +13,7 @@ test('it renders the page if we are not testing login', () => {
 test('it renders the page if they are logged in and we are testing logins', () => {
   jest.spyOn(auth, 'isLoggedIn').mockImplementation(() => true)
 
-  const wrapper = shallow(<PrivateRoute location={ {} } testLogin={true}>TEXT</PrivateRoute>)
+  const wrapper = shallow(<PrivateRoute location={{}} testLogin={true}>TEXT</PrivateRoute>)
 
   expect(wrapper.text()).toEqual('TEXT')
 })
@@ -21,7 +21,7 @@ test('it renders the page if they are logged in and we are testing logins', () =
 test('it navigates to the login page if they are not logged in and we are testing logins', () => {
   jest.spyOn(auth, 'isLoggedIn').mockImplementation(() => false)
 
-  shallow(<PrivateRoute location={ {} } testLogin={true}>TEXT</PrivateRoute>)
+  shallow(<PrivateRoute location={{}} testLogin={true}>TEXT</PrivateRoute>)
 
   expect(Gatsby.navigate).toHaveBeenCalledWith('/login')
 })
