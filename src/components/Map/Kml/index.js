ba9ebe6ecaa99cc4684d280f2191df4d845bbc4d
Jon Hartzler <jon.hartzler@gmail.comcoke>
2019-06-12 12:37:58 -0400

    Hound

--- src/components/Map/Kml/index.js
+++ src/components/Map/Kml/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Map from 'components/Map'
-const { KmlLayer } = require("react-google-maps")
+const { KmlLayer } = require('react-google-maps')
 
 // http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml
 const KmlMap = ({ center, kmlFile, defaultZoom }) => {
