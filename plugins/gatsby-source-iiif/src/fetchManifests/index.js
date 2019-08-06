e85adfde7f9a9828b2f4bcc429910a1635c8e3ae
Jon Hartzler <jon.hartzler@gmail.com>
2019-08-06 08:21:15 -0400

    reduce complexity

--- plugins/gatsby-source-iiif/src/fetchManifests/index.js
+++ plugins/gatsby-source-iiif/src/fetchManifests/index.js
@@ -4,31 +4,38 @@ const traverseAndFixData = require('./fixIiifJson')
 module.exports = async (urls) => {
   const result = {}
 
-  const recursiveProcess = async (url) => {
+  const recursiveFetchManifest = async (url, parentId) => {
     console.log('Fetching:', url)
     return fetch(url)
       .then(response => response.json())
       .then((data) => {
-        let children = []
-        if (data.type.toLowerCase() === 'collection') {
-          children = data.items.map(manifest => manifest.id)
-        }
+        data['parentId'] = parentId
+        data['slug'] = getFilenameFromId(data['id'])
 
-        const childRequests = children.map(url => {
-          return recursiveProcess(url, data['id'])
-        })
-
-        const filename = data['id'].replace(/http[s]?:\/\/.*?\//, '').replace('/manifest', '').replace('collection/', '')
-        data['slug'] = filename
         result[data['id']] = {}
         result[data['id']] = data
 
-        return Promise.all(childRequests)
+        return recursiveFetchChildren(data)
       }).catch(err => console.log('fetch error', err))
   }
 
+  const getFilenameFromId = (id) => {
+    return id.replace(/http[s]?:\/\/.*?\//, '').replace('/manifest', '').replace('collection/', '')
+  }
+
+  const recursiveFetchChildren = (manifest) => {
+    let children = []
+    if (manifest.type.toLowerCase() === 'collection') {
+      children = manifest.items.map(manifest => manifest.id)
+    }
+
+    return Promise.all(children.map(url => {
+      return recursiveFetchManifest(url, manifest['id'])
+    }))
+  }
+
   const expandManifestTree = (manifest) => {
-    console.log('Expanding', manifest.id)
+    console.log('Expanding:', manifest.id)
     manifest.items = manifest.items.map((item) => {
       if (item.type.toLowerCase() === 'manifest') {
         return result[item.id]
@@ -41,7 +48,7 @@ module.exports = async (urls) => {
   }
 
   const requests = urls.map(url => {
-    return recursiveProcess(url)
+    return recursiveFetchManifest(url)
   })
 
   await Promise.all(requests)
