b62643f25f176ffe2e550668a7bf4430d7a38a3e
Dan Wolfe <dwolfe2@nd.edu>
2019-08-12 13:01:15 -0400

    Update test.js

--- src/components/Shared/SearchTools/SearchFilterBox/test.js
+++ src/components/Shared/SearchTools/SearchFilterBox/test.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import SearchFilterBox from './'
-// import {
-//   DataSearch,
-//   SelectedFilters,
-// } from '@appbaseio/reactivesearch'
+import {
+  SearchBox,
+  SelectedFilters,
+  ResetFilters,
+} from 'searchkit'
 
 test('SearchPreMain', () => {
   const wrapper = shallow(<SearchFilterBox />)
-  // expect(wrapper.find('.searchHead').exists()).toBeTruthy()
-  // expect(wrapper.find(DataSearch).exists()).toBeTruthy()
-  // expect(wrapper.find(SelectedFilters).exists()).toBeTruthy()
+  expect(wrapper.find('.searchHead').exists()).toBeTruthy()
+  expect(wrapper.find(SearchBox).exists()).toBeTruthy()
+  expect(wrapper.find(SelectedFilters).exists()).toBeTruthy()
+  expect(wrapper.find(ResetFilters).exists()).toBeTruthy()
 })
