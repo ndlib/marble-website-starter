4f5e9aa34ade5ce0831cb083b94a3b1d153a83ee
Dan Wolfe <dwolfe2@nd.edu>
2019-08-21 11:28:22 -0400

    try lowering test coverage to make code climate mad

--- @hesburgh-wse/gatsby-theme-marble/src/components/Internal/Seo/test.js
+++ @hesburgh-wse/gatsby-theme-marble/src/components/Internal/Seo/test.js
@@ -37,75 +37,75 @@ describe('Seo', () => {
     expect(actual).toEqual('Default Title')
   })
   test('getImage', () => {
-    const frontmatter = {
-      iiifJson: {
-        thumbnail: [
-          { id: 'thumbnail.jpg' },
-        ],
-      },
-    }
-    const defaultImage = {
-      publicURL: 'defaultImage.jpg',
-    }
-    let actual = getImage(frontmatter, defaultImage)
-    expect(actual).toEqual('thumbnail.jpg')
-    actual = getImage({}, defaultImage)
-    expect(actual).toEqual('defaultImage.jpg')
+    // const frontmatter = {
+    //   iiifJson: {
+    //     thumbnail: [
+    //       { id: 'thumbnail.jpg' },
+    //     ],
+    //   },
+    // }
+    // const defaultImage = {
+    //   publicURL: 'defaultImage.jpg',
+    // }
+    // let actual = getImage(frontmatter, defaultImage)
+    // expect(actual).toEqual('thumbnail.jpg')
+    // actual = getImage({}, defaultImage)
+    // expect(actual).toEqual('defaultImage.jpg')
   })
-  test('getDescription', () => {
-    const description = 'Direct Description'
-    const frontmatter = {
-      iiifJson: {
-        summary: 'Manifest Description',
-      },
-    }
-    const siteMetadata = {
-      description: 'Site Description',
-    }
-    const longDescription = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
-
-    let actual = getDescription(description, frontmatter, siteMetadata)
-    expect(actual).toEqual('Direct Description')
-    actual = getDescription(null, frontmatter, siteMetadata)
-    expect(actual).toEqual('Manifest Description')
-    actual = getDescription(null, null, siteMetadata)
-    expect(actual).toEqual('Site Description')
-    actual = getDescription(longDescription, frontmatter, siteMetadata)
-    expect(actual).toEqual('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis…')
-  })
-  test('getAuthor', () => {
-    const siteMetadata = {
-      author: 'Bill Nye the Science Guy',
-    }
-    let actual = getAuthor('Bill Guy the Science Nye', siteMetadata)
-    expect(actual).toEqual('Bill Guy the Science Nye')
-    actual = getAuthor('', siteMetadata)
-    expect(actual).toEqual('Bill Nye the Science Guy')
-  })
-  test('getDescriptionFromMetadata', () => {
-    const siteMetadata = {
-      languages: {
-        default: 'tlh',
-      },
-    }
-    const frontmatter = {
-      iiifJson: {
-        metadata: [
-          {
-            label: {
-              tlh: ['Summary'],
-            },
-            value: {
-              tlh: ['You are a disappointment to the empire.'],
-            },
-          },
-        ],
-      },
-    }
-
-    let actual = getDescriptionFromMetadata(frontmatter, siteMetadata)
-    expect(actual).toEqual('You are a disappointment to the empire.')
-    actual = getDescriptionFromMetadata({}, siteMetadata)
-    expect(actual).toEqual(null)
-  })
+  // test('getDescription', () => {
+  //   const description = 'Direct Description'
+  //   const frontmatter = {
+  //     iiifJson: {
+  //       summary: 'Manifest Description',
+  //     },
+  //   }
+  //   const siteMetadata = {
+  //     description: 'Site Description',
+  //   }
+  //   const longDescription = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
+  //
+  //   let actual = getDescription(description, frontmatter, siteMetadata)
+  //   expect(actual).toEqual('Direct Description')
+  //   actual = getDescription(null, frontmatter, siteMetadata)
+  //   expect(actual).toEqual('Manifest Description')
+  //   actual = getDescription(null, null, siteMetadata)
+  //   expect(actual).toEqual('Site Description')
+  //   actual = getDescription(longDescription, frontmatter, siteMetadata)
+  //   expect(actual).toEqual('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis…')
+  // })
+  // test('getAuthor', () => {
+  //   const siteMetadata = {
+  //     author: 'Bill Nye the Science Guy',
+  //   }
+  //   let actual = getAuthor('Bill Guy the Science Nye', siteMetadata)
+  //   expect(actual).toEqual('Bill Guy the Science Nye')
+  //   actual = getAuthor('', siteMetadata)
+  //   expect(actual).toEqual('Bill Nye the Science Guy')
+  // })
+  // test('getDescriptionFromMetadata', () => {
+  //   const siteMetadata = {
+  //     languages: {
+  //       default: 'tlh',
+  //     },
+  //   }
+  //   const frontmatter = {
+  //     iiifJson: {
+  //       metadata: [
+  //         {
+  //           label: {
+  //             tlh: ['Summary'],
+  //           },
+  //           value: {
+  //             tlh: ['You are a disappointment to the empire.'],
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   }
+  //
+  //   let actual = getDescriptionFromMetadata(frontmatter, siteMetadata)
+  //   expect(actual).toEqual('You are a disappointment to the empire.')
+  //   actual = getDescriptionFromMetadata({}, siteMetadata)
+  //   expect(actual).toEqual(null)
+  // })
 })
