c16041d0715e8b9c66384c27d50b3e22919e92f3
Dan Wolfe <dwolfe2@nd.edu>
2019-08-21 10:43:15 -0400

    test lowing test coverage

--- @hesburgh-wse/gatsby-theme-marble/src/components/Layout/ConstructionBanner/test.js
+++ @hesburgh-wse/gatsby-theme-marble/src/components/Layout/ConstructionBanner/test.js
@@ -4,6 +4,6 @@ import ConstructionBanner from './'
 import packageJson from '../../../../package.json'
 
 test('ConstructionBanner', () => {
-  const wrapper = shallow(<ConstructionBanner />)
-  expect(wrapper.find('.versionText').text()).toEqual(`Alpha Release: ${packageJson.version}`)
+  // const wrapper = shallow(<ConstructionBanner />)
+  // expect(wrapper.find('.versionText').text()).toEqual(`Alpha Release: ${packageJson.version}`)
 })
