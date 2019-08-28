0d0801d3cacb72af1ce722459066a6f5ea93ddeb
Dan Wolfe <dwolfe2@nd.edu>
2019-08-28 13:45:19 -0400

    Stupid graphql timing issue makes sharing queries difficult.

--- @hesburgh-wse/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/HamburgerButton/index.js
+++ @hesburgh-wse/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/HamburgerButton/index.js
@@ -2,11 +2,28 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import typy from 'typy'
-import { menuQuery, findNavInData } from 'components/Shared/Menu'
+import { findNavInData } from 'components/Shared/Menu'
 import { jsx } from 'theme-ui'
 import hamburgerIcon from 'assets/icons/svg/baseline-menu-24px-white.svg'
+export const menuQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        menus {
+          id
+          label
+          items {
+            id
+            link
+            label
+          }
+        }
+      }
+    }
+  }
+`
 
 const HamburgerButton = ({ onClick, onBlur, className }) => {
   const { site } = useStaticQuery(menuQuery)
