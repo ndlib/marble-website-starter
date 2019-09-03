e442f4af23106b6bd500ec856fe49a8792d6877f
Dan Wolfe <dwolfe2@nd.edu>
2019-09-03 13:31:15 -0400

    unit test

---
+++ @hesburgh-wse/gatsby-theme-marble/src/components/Shared/ManifestImage/test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import ManifestImage from './'
+import Image from 'components/Shared/Image'
+describe('ManifestImage', () => {
+  const iiifManifest = {
+    description: 'Manifest Description',
+    name: 'Manifest Name',
+  }
+  const className = 'myClass'
+  test('props from manifest', () => {
+    const wrapper = shallow(<ManifestImage iiifManifest={iiifManifest} className={className} />)
+    expect(wrapper.find('.myClass').props().alt).toEqual('Manifest Description')
+    expect(wrapper.find(Image).props().title).toEqual('Manifest Name')
+  })
+  test('props from props', () => {
+    const wrapper = shallow(<ManifestImage iiifManifest={iiifManifest} className={className} title='propTitle' alt='propAlt' />)
+    expect(wrapper.find('.myClass').props().alt).toEqual('propAlt')
+    expect(wrapper.find(Image).props().title).toEqual('propTitle')
+  })
+})
