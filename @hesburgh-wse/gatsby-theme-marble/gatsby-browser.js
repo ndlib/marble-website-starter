9fa59f636b7e159b9ebade19981005c1f650bde2
Dan Wolfe <dwolfe2@nd.edu>
2019-09-11 10:57:08 -0400

    * fix bug * add `serve` command to `package.json` files

--- @hesburgh-wse/gatsby-theme-marble/gatsby-browser.js
+++ @hesburgh-wse/gatsby-theme-marble/gatsby-browser.js
@@ -2,12 +2,12 @@ import 'searchkit/release/theme.css'
 import './src/styles/global.css'
 export { default as wrapRootElement } from './src/store/ReduxWrapper'
 
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     `This application has been updated. ` +
+//       `Reload to display the latest version?`
+//   )
+//   if (answer === true) {
+//     window.location.reload()
+//   }
+// }
