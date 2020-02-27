91362663152bc237d138553c5e456a86c067d234
Jon Hartzler <jon.hartzler@gmail.com>
2020-02-27 14:43:21 -0500

    update search

--- scripts/gatsby-source-iiif/getSchema.js
+++ scripts/gatsby-source-iiif/getSchema.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require(`path`)
 const fetch = require('node-fetch')
 
-const loadIIIFData = () => {
-  const contents = fs.readFileSync(path.join(__dirname, '/../../site/content/json/iiif/iiif.json'))
+const loadManifestsFile = () => {
+  const contents = fs.readFileSync(path.join(__dirname, '/../../site/content/manifests.json'))
   return JSON.parse(contents)
 }
 
@@ -11,7 +11,8 @@ const fetchData = async (seeAlso) => {
   const finalResult = []
   await Promise.all(seeAlso.map(item => {
     console.log('Processing: ' + item)
-    const result = fetch(item)
+    const url = 'https://presentation-iiif.library.nd.edu/' + item + '/nd'
+    const result = fetch(url)
       .then(response => response.json())
       .then(data => {
         finalResult.push(data)
@@ -32,29 +33,10 @@ const fetchData = async (seeAlso) => {
   return finalResult
 }
 
-const getSchemaSeeAlsoList = (rawIIIF) => {
-  const seeAlsoSchema = []
-  rawIIIF.forEach(function (entry) {
-    if ('seeAlso' in entry) {
-      const seeAlso = entry.seeAlso
-      seeAlso.forEach(function (eachSeeAlso) {
-        if (eachSeeAlso.profile === 'https://schema.org/') {
-          console.log('Extracting: ' + eachSeeAlso.id)
-          seeAlsoSchema.push(eachSeeAlso.id)
-        }
-      })
-    }
-  })
-  return seeAlsoSchema
-}
 // eslint-disable-next-line
 new Promise(async (resolve, reject) => {
-  resolve()
-  return
-
-  const rawIIIF = await loadIIIFData()
-  const seeAlso = await getSchemaSeeAlsoList(rawIIIF)
-  const fetchResult = await fetchData(seeAlso)
+  const rawIds = await loadManifestsFile()
+  const fetchResult = await fetchData(rawIds.manifest_ids)
   if (!fetchResult) {
     reject(fetchResult)
   }
