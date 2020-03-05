72d7db042a942e4bff05c767f9944695273e21d8
Jon Hartzler <jon.hartzler@gmail.com>
2020-03-05 11:03:30 -0500

    add all page.

---
+++ @ndlib/gatsby-theme-marble/src/pages/all.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Link from 'components/Internal/Link'
+
+export const AllPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.slug.match(/item/)) // You can filter your posts based on some criteria
+    .map(edge => <li><Link to={edge.node.frontmatter.slug} >{edge.node.frontmatter.title}</Link></li>)
+
+  return (
+    <div>
+      <h1>All Current items</h1>
+      <ul>{Posts}</ul>
+    </div>
+  )
+}
+
+export default AllPage
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`
