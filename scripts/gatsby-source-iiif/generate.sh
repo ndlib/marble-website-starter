65938cdcb70c59ca93be450d5b732b9dd79bb7c3
Ryan Doughty <rdought1@nd.edu>
2020-03-16 13:59:00 -0400

    remove dotenv from script

--- scripts/gatsby-source-iiif/generate.sh
+++ scripts/gatsby-source-iiif/generate.sh
@@ -3,4 +3,4 @@ site="../../site"
 node getManifests.js ${site}
 node generateMD.js ${site}
 node getSchema.js ${site}
-node -r dotenv/config indexSearch.js ${site} dotenv_config_path=${DOTENV_CONFIG}
+node indexSearch.js ${site}
