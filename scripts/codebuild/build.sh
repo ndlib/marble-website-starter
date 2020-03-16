5dd6b9d4cda3308b5da4a08a98e551a765a6033b
Ryan Doughty <rdought1@nd.edu>
2020-03-16 13:49:03 -0400

    fix pathing

--- scripts/codebuild/build.sh
+++ scripts/codebuild/build.sh
@@ -4,7 +4,7 @@ reset=`tput sgr0`
 echo "${magenta}----- BUILD -----${reset}"
 
 # generated in install.sh
-ENV_FILE="${PWD}.env"
+ENV_FILE="${PWD}/.env"
 echo "BUILD ENV: ${ENV_FILE}"
 source ${ENV_FILE}
 export $(cut -d= -f1 ${ENV_FILE})
