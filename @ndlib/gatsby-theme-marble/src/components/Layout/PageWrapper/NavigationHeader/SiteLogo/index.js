35501be612b556355189163e55318373ae133ef1
Dan Wolfe <dwolfe2@nd.edu>
2020-03-19 15:58:53 -0400

    move sx to external file

--- @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/SiteLogo/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/SiteLogo/index.js
@@ -5,6 +5,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 import Link from 'components/Internal/Link'
 import typy from 'typy'
 import { jsx } from 'theme-ui'
+import sx from './sx'
 import siteLogo from 'assets/images/siteLogo.png'
 
 export const SiteLogo = () => {
@@ -24,32 +25,15 @@ export const SiteLogo = () => {
   return (
     <Link
       to='/'
-      sx={{
-        display: 'inline-flex',
-        height: '70px',
-        margin: '0',
-        textDecoration: 'none',
-      }}>
+      sx={sx.link}>
       {
         site.siteMetadata.hideLogo ? (
           <div
-            sx={{
-              fontFamily: 'logo',
-              fontSize: '24px',
-              lineHeight: '70px',
-              minWidth: '240px',
-              textAlign: 'center',
-              verticalAlign: 'middle',
-            }}
+            sx={sx.text}
           >{title}</div>
         ) : (
           <img
-            sx={{
-              height: '50px',
-              width: '200px',
-              maxWidth: '200px',
-              margin: '20px 0',
-            }}
+            sx={sx.image}
             alt={title}
             title={title}
             src={siteLogo}
---
+++ @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/SiteLogo/sx.js
@@ -0,0 +1,22 @@
+module.exports = {
+  link: {
+    display: 'inline-flex',
+    height: '70px',
+    margin: '0',
+    textDecoration: 'none',
+  },
+  text: {
+    fontFamily: 'logo',
+    fontSize: '24px',
+    lineHeight: '70px',
+    minWidth: '240px',
+    textAlign: 'center',
+    verticalAlign: 'middle',
+  },
+  image: {
+    height: '50px',
+    width: '200px',
+    maxWidth: '200px',
+    margin: '20px 0',
+  },
+}
