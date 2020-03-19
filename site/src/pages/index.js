5510dd1070c47925a891a466a5409e086aeb656a
Dan Wolfe <dwolfe2@nd.edu>
2020-03-19 14:55:42 -0400

    no like actually fix it

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import typy from 'typy'
 import Layout from 'components/Layout'
 import Seo from 'components/Internal/Seo'
 import IndexPage from 'components/Pages/IndexPage'
 
 const Home = ({ data, location }) => {
-  const title = typy(data, 'site.siteMetadata.title').safeString || null
-
   return (
     <Layout
-      title={title}
       location={location}
     >
       <Seo
