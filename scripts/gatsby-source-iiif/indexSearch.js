0f337eed525b9821d5ccf013d2492a15810f630b
Jon Hartzler <jon.hartzler@gmail.com>
2020-03-23 11:28:09 -0400

    Update indexSearch.js

--- scripts/gatsby-source-iiif/indexSearch.js
+++ scripts/gatsby-source-iiif/indexSearch.js
@@ -14,10 +14,9 @@ console.log('domain: ' + domain)
 console.log('env index: ' + process.env.SEARCH_INDEX)
 console.log('site index: ' + siteIndex)
 
-return
 const appConfig = process.env.APP_CONFIG
 if (appConfig === 'local') {
-
+  return
 }
 
 if (!domain || !siteIndex || domain === 'travis-test-no-index' || siteIndex === 'travis-test-no-index') {
