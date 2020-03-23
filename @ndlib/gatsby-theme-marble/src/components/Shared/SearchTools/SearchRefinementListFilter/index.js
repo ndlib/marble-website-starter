6c5cf44e11ca88fa9f44f50555975df4a4d65cf9
jmille79 <jmille79@nd.edu>
2020-03-23 14:16:43 -0400

    Fix Hound

--- @ndlib/gatsby-theme-marble/src/components/Shared/SearchTools/SearchRefinementListFilter/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/SearchTools/SearchRefinementListFilter/index.js
@@ -28,7 +28,7 @@ const SearchRefinementListFilter = ({ field, label, operator, defaultSearch, siz
       title={label}
       operator={operator}
       bucketsTransform={listOrder}
-      size={parseInt(size)}
+      size={parseInt(size, 10)}
     />
   )
 }
