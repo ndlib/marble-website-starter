31bdbf22cc8253590b074390b98584ab2171a048
Jon Hartzler <jon.hartzler@gmail.com>
2020-03-27 11:30:14 -0400

    local build works with download

--- scripts/gatsby-source-iiif/getManifests.js
+++ scripts/gatsby-source-iiif/getManifests.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const fetchData = require('./src/fetchManifests')
 const path = require(`path`)
 const directory = process.argv.slice(2)[0]
+// temp
+const fetch = require('node-fetch')
 
 const loadManifestsFile = () => {
   const contents = fs.readFileSync(path.join(directory, '/content/manifests.json'))
@@ -12,7 +14,7 @@ const ensureDirectoryStructure = async () => {
   return Promise.all([
     fs.promises.mkdir(path.join(directory, '/content/json/iiif/'), { recursive: true }),
     fs.promises.mkdir(path.join(directory, '/content/markdown/iiif/'), { recursive: true }),
-    fs.promises.mkdir(path.join(directory, '/content/json/search/'), { recursive: true }),
+    fs.promises.mkdir(path.join(directory, '/content/json/items/'), { recursive: true }),
     fs.promises.mkdir(path.join(directory, '/content/markdown/browse/'), { recursive: true }),
   ])
 }
