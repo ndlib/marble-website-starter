9a6f4f4a966eaca46412e7574f2a144d9cca2f40
jmille79 <jmille79@nd.edu>
2020-03-31 17:06:18 -0400

    update

--- @ndlib/gatsby-theme-marble/src/components/Shared/SearchTools/SearchRefinementListFilter/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/SearchTools/SearchRefinementListFilter/test.js
@@ -25,15 +25,6 @@ const list = [
   { key: '17th Century', doc_count: 3 },
   { key: '20th Century', doc_count: 1 }]
 
-test('ListOrderAZ', () => {
-  const testList = listOrder(list, 'a-z')
-  expect(testList).toMatchObject([
-    { key: '17th Century', doc_count: 3 },
-    { key: '18th Century', doc_count: 4 },
-    { key: '19th Century', doc_count: 7 },
-    { key: '20th Century', doc_count: 1 },
-    { key: 'undated', doc_count: 4 }])
-})
 test('ListOrderNotAZ', () => {
   const testList = listOrder(list, 'not-a-z')
   expect(testList).toMatchObject([
@@ -42,4 +33,14 @@ test('ListOrderNotAZ', () => {
     { key: 'undated', doc_count: 4 },
     { key: '17th Century', doc_count: 3 },
     { key: '20th Century', doc_count: 1 }])
+})
+
+test('ListOrderAZ', () => {
+  const testList = listOrder(list, 'a-z')
+  expect(testList).toMatchObject([
+    { key: '17th Century', doc_count: 3 },
+    { key: '18th Century', doc_count: 4 },
+    { key: '19th Century', doc_count: 7 },
+    { key: '20th Century', doc_count: 1 },
+    { key: 'undated', doc_count: 4 }])
 })
