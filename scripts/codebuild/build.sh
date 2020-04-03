361c9ce1f98af09819acdd200f9deefdf8f21253
Jon Hartzler <jon.hartzler@gmail.com>
2020-04-03 09:40:17 -0400

    revert further

--- scripts/codebuild/build.sh
+++ scripts/codebuild/build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 magenta=`tput setaf 5`
 reset=`tput sgr0`
@@ -6,13 +7,11 @@ echo "${magenta}----- BUILD -----${reset}"
 # generated in install.sh
 ENV_FILE="${PWD}/.env"
 echo "BUILD ENV: ${ENV_FILE}"
-echo "$(cat ${ENV_FILE})"
-
 source ${ENV_FILE}
 export $(cut -d= -f1 ${ENV_FILE})
+echo "$(cat ${ENV_FILE})"
 echo "BUILD SEARCH URL: ${SEARCH_URL}"
 echo "BUILD SEARCH INDEX: ${SEARCH_INDEX}"
-export SEARCH_URL=${SEARCH_URL}
-export SEARCH_INDEX=${SEARCH_INDEX}
+
 # build
-yarn workspace site build || { echo "\e[31mGatsby build failed${reset}"; exit 1; }
+yarn workspace site build || { echo "Gatsby build failed" ;exit 1; }
--- scripts/codebuild/pre_build.sh
+++ scripts/codebuild/pre_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 magenta=`tput setaf 5`
 reset=`tput sgr0`
+
 echo "${magenta}----- TESTS ------${reset}"
 
-yarn workspace @ndlib/gatsby-theme-marble test  || { echo "\e[31mUnit Tests Failed${reset}"; exit 1; }
+yarn workspace @ndlib/gatsby-theme-marble test || { echo "Unit Tests Failed"; exit 1; }
