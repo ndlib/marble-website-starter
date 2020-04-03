b3997f3f786641d1d10ba3c2b0b0170646d1be03
Jon Hartzler <jon.hartzler@gmail.com>
2020-04-03 09:52:44 -0400

    start removing till it works. 1

--- scripts/gatsby-source-iiif/generate.sh
+++ scripts/gatsby-source-iiif/generate.sh
@@ -1,9 +1,20 @@
 yarn install
 site="../../site"
 node getManifests.js ${site}
+echo "Get Manifests"
+echo $?
 # node downloadJsonAssets.js ${site}
 node generateMD.js ${site}
-node getSchema.js ${site}
+echo "Generate MD"
+echo $?
+
+#node getSchema.js ${site}
+#echo "Get Scheme"
+#echo $?
+
 if [ "${1}" != "local" ]; then
-  node indexSearch.js ${site}
+  #node indexSearch.js ${site}
+  #echo "Index"
+  #echo $?
+
 fi
