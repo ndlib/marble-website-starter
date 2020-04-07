d807b25457b7e416206673f928a1fce522f77d78
jmille79 <jmille79@nd.edu>
2020-04-07 15:29:42 -0400

    Suggested change

--- site/src/@ndlib/gatsby-theme-marble/components/Shared/SearchTools/SearchFilterBox/index.js
+++ site/src/@ndlib/gatsby-theme-marble/components/Shared/SearchTools/SearchFilterBox/index.js
@@ -4,7 +4,7 @@ import {
   SelectedFilters,
   ResetFilters,
 } from 'searchkit'
-import HeroBox from 'components/shared/HeroBox'
+import HeroBox from '../../HeroBox'
 
 import banner from 'assets/images/banner.swirl.png'
 
