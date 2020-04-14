79cb152ce38217e2d27f99fc9d1b65b823622f7e
Jon Hartzler <jon.hartzler@gmail.com>
2020-04-14 10:28:08 -0400

    add tests for circa

--- scripts/gatsby-source-iiif/src/realDatesFromCatalogedDates/test.js
+++ scripts/gatsby-source-iiif/src/realDatesFromCatalogedDates/test.js
@@ -1,31 +1,7 @@
 const realDatesFromCatalogedDates = require('./')
 
-test('takes a string year and gets a tag for it', () => {
+test('takes a string year(s) and gets a highest and lowest values for it', () => {
   [
-    /*
-    ['1790-1890', '18th Century', '19th Century'],
-    ['1937.', '20th Century'],
-    ['ca. 1899-1900', '19th Century', '20th Century'],
-    ['19--?', '20th Century'],
-    ['176-?', '18th Century'],
-    [1342, '14th Century'],
-    ['1900', '20th Century'],
-    ['1899', '19th Century'],
-    ['2003', '21st Century'],
-    ['', 'undated'],
-    [null, 'undated'],
-    [[], 'undated'],
-    [['1937.'], '20th Century'], // takes dates in an array form
-    [['ca. 1899-1900'], '19th Century', '20th Century'],
-    ['not a recognized date', 'undated'],
-    ['250-400', '3rd Century', '4th Century', '5th Century'],
-    */
-
-    // awaiting the meeting to discuss.
-    // ['0'], '1st Century']
-    // ['1950', 1970, '2001'], '20th Century', 21st Century']
-    // ['mid-20th Century'], '20th Century']
-    // ['no date'], 'undated']
     ['1790-1890', 1790, 1890],
     ['19--?', 1900, 1999],
     ['1745-1750; 1780; 1790-1800', 1745, 1800],
@@ -58,6 +34,16 @@ test('It sets undated for undated dates', () => {
   ].forEach((testData) => {
     const result = realDatesFromCatalogedDates(testData[0])
     expect(result.undated).toEqual(true)
+  })
+})
+
+test('It sets circa for circa dates', () => {
+  // test undated
+  [
+    ['ca. 1899-1900', ['ca. 1899-1900', '1450'], 'c. 1523-3212'],
+  ].forEach((testData) => {
+    const result = realDatesFromCatalogedDates(testData[0])
+    expect(result.circa).toEqual(true)
   })
 })
 
