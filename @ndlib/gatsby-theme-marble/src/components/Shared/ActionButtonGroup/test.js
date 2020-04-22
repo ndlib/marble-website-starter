9cf57979c32049028581267196c2ea628d930685
Dan Wolfe <dwolfe2@nd.edu>
2020-04-22 12:39:28 -0400

    unit test

--- @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/test.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import ActionButtonGroup from './'
+import ActionButtonGroup, { printAction } from './'
 import ActionButton from './ActionButton'
+import BookmarkGroup from './BookmarkGroup'
+import ActionModal from 'components/Internal/ActionModal'
+import DownloadModalContent from './DownloadModalContent'
+import ShareModalContent from './ShareModalContent'
 
 const print = jest.fn()
 const window = global.window || {}
-Object.defineProperty(window, 'print', print)
+Object.defineProperty(window, 'print', { value: print })
 
 const manifest = {
   id: 'https://iiif.iiif',
   slug: 'item/3',
 }
 const wrapper = shallow(<ActionButtonGroup iiifManifest={manifest} />)
+describe('ActionButtonGroup', () => {
+  test('Renders 4 actions buttons and an wrapper', () => {
+    expect(wrapper.find('actionButton')).toBeTruthy()
+    expect(wrapper.find(BookmarkGroup).exists()).toBeTruthy()
+    expect(wrapper.find(ActionButton).length).toEqual(3)
+    expect(wrapper.find(ActionModal).length).toEqual(2)
+    expect(wrapper.find(ShareModalContent).exists()).toBeTruthy()
+    expect(wrapper.find(DownloadModalContent).exists()).toBeTruthy()
+  })
 
-test('Renders 4 actions buttons and an wrapper', () => {
-  expect(wrapper.find('actionButton')).toBeTruthy()
-  expect(wrapper.find(ActionButton).length).toEqual(3)
+  test('printAction', () => {
+    printAction()
+    expect(print).toHaveBeenCalled()
+  })
 })
