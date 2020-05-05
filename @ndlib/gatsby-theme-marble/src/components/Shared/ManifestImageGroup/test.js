153a4c3f468f0619b3ba73a9e27077bb78b1424a
Dan Wolfe <dwolfe2@nd.edu>
2020-05-05 13:17:16 -0400

    MARBLE-830, MARBLE-831 Manifest main image * Return of the gray box that maxes the size of the image. * Use the first full image as specified in the manifest, this is not always optimal, but is the only way to enforce not enlarging the image beyond what is specified. * Update unit test

--- @ndlib/gatsby-theme-marble/src/components/Shared/ManifestImageGroup/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ManifestImageGroup/test.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import ManifestImageGroup from './'
 import ViewerLink from './ViewerLink'
-import ManifestImage from 'components/Shared/ManifestImage'
 import ExpandIcon from './ExpandIcon'
 import ItemAlternateViews from './ItemAlternateViews'
 
@@ -11,13 +10,24 @@ console.error = jest.fn()
 const manifest = {
   id: 'id',
   slug: 'slug',
+  items: [{
+    items: [{
+      items: [{
+        body: {
+          id: '/image.png',
+        },
+      }],
+    }],
+  }],
 }
-const wrapper = shallow(<ManifestImageGroup iiifManifest={manifest} location={{}} />)
+const wrapper = mount(<ManifestImageGroup iiifManifest={manifest} location={{}} />)
 
 test('ManifestImageGroup', () => {
   expect(wrapper.find('section').exists()).toBeTruthy()
   expect(wrapper.find(ViewerLink).exists()).toBeTruthy()
-  expect(wrapper.find(ManifestImage).exists()).toBeTruthy()
   expect(wrapper.find(ExpandIcon).exists()).toBeTruthy()
+  expect(wrapper.findWhere(img => {
+    return img.prop('src') === '/image.png'
+  }).exists()).toBeTruthy()
   expect(wrapper.find(ItemAlternateViews).exists()).toBeTruthy()
 })
