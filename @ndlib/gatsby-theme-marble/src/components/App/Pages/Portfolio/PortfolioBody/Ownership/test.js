982ae252d5d475aaf6b786b29a3baaff08fe169d
Dan Wolfe <dwolfe2@nd.edu>
2020-05-19 11:59:10 -0400

    working example of testing with context

--- @ndlib/gatsby-theme-marble/src/components/App/Pages/Portfolio/PortfolioBody/Ownership/test.js
+++ @ndlib/gatsby-theme-marble/src/components/App/Pages/Portfolio/PortfolioBody/Ownership/test.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { Ownership } from './'
+import * as PC from 'context/PortfolioContext'
 import VisibilityLabel from 'components/Internal/VisibilityLabel'
 import UserCartouche from 'components/Internal/UserCartouche'
 
-console.error = jest.fn()
 describe('Ownership', () => {
   test('isOwner', () => {
+    jest.spyOn(PC, 'usePortfolioContext').mockImplementationOnce(() => {
+      return {
+        portfolio: {
+          privacy: 'public',
+        },
+      }
+    })
     const wrapper = shallow(<Ownership isOwner />)
-    expect(wrapper.find(VisibilityLabel).exists()).toBeTruthy()
+    expect(wrapper.find(VisibilityLabel).props().visibility).toEqual('public')
   })
 
   test('not isOwner', () => {
+    jest.spyOn(PC, 'usePortfolioContext').mockImplementationOnce(() => {
+      return {
+        portfolio: {
+          userId: 'pete',
+        },
+      }
+    })
     const wrapper = shallow(<Ownership />)
-    expect(wrapper.find(UserCartouche).exists()).toBeTruthy()
+    expect(wrapper.find(UserCartouche).props().user).toEqual({ uuid: 'pete' })
   })
 })
