35eb5041451ba5035c697830ccd10b255a29a48b
Jon Hartzler <jon.hartzler@gmail.com>
2020-05-20 08:22:37 -0400

    update search to put errors in the log from bulk indexes

--- scripts/gatsby-source-iiif/indexSearch.js
+++ scripts/gatsby-source-iiif/indexSearch.js
@@ -110,23 +110,45 @@ const getSearchDataFromManifest = (manifest) => {
   search['allMetadata'] += ' ' + search.centuryTag.join(' ')
   search['allMetadata'] += ' ' + search.themeTag.join(' ')
 
+  if (manifest.id === '005069928') {
+    console.log(search)
+  }
   return search
 }
 
 const indexToElasticSearch = async (searchData) => {
   console.log('Indexing')
   // convert the data to bulk index format
-  const indexData = []
+  const body = []
   searchData.forEach((manifest) => {
-    indexData.push({ index:  { _index: siteIndex, _type: '_doc', _id: manifest.id } })
-    indexData.push(manifest)
+    body.push({ index:  { _index: siteIndex, _type: '_doc', _id: manifest.id } })
+    body.push(manifest)
   })
 
-  await client.bulk({
-    body: indexData,
-  }).catch((e) => {
-    console.log(e)
-  })
+  const { body: bulkResponse } = await client.bulk({ refresh: true, body })
+
+  if (bulkResponse.errors) {
+    const erroredDocuments = []
+    // The items array has the same order of the dataset we just indexed.
+    // The presence of the `error` key indicates that the operation
+    // that we did for the document has failed.
+    bulkResponse.items.forEach((action, i) => {
+      const operation = Object.keys(action)[0]
+      if (action[operation].error) {
+        erroredDocuments.push({
+          // If the status is 429 it means that you can retry the document,
+          // otherwise it's very likely a mapping error, and you should
+          // fix the document before to try it again.
+          status: action[operation].status,
+          error: action[operation].error,
+          operation: body[i * 2],
+          document: body[i * 2 + 1],
+        })
+      }
+    })
+    console.log(erroredDocuments)
+  }
+
   console.log('Finished Index')
 }
 
@@ -201,8 +223,8 @@ new Promise(async (resolve, reject) => {
 
   await setupIndex()
   await indexToElasticSearch(writeData)
-  // console.log('Writing Search Data to gatsby')
-  // fs.writeFileSync(path.join(writeDirectory, 'search.json'), JSON.stringify(writeData))
+  console.log('Writing Search Data to gatsby')
+  fs.writeFileSync(path.join(writeDirectory, 'search.json'), JSON.stringify(writeData))
 
   resolve()
 }).then(() => {
