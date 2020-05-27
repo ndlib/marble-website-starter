5dced1b071363edfa84e54df1f0d7c2bac306d4a
Dan Wolfe <dwolfe2@nd.edu>
2020-05-27 13:14:52 -0400

    sx tweak

--- @ndlib/gatsby-theme-marble/src/components/Internal/ReturnToSearch/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Internal/ReturnToSearch/index.js
@@ -15,6 +15,7 @@ export const ReturnToSearch = ({ location }) => {
       <BaseStyles>
         <Link
           to={`/search${location.state.referal.query}`}
+          sx={sx.link}
         >
           <svg xmlns='http://www.w3.org/2000/svg'
             height='16'
@@ -23,7 +24,8 @@ export const ReturnToSearch = ({ location }) => {
             sx={sx.svg}>
             <path d='M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z' fill={iconColor} />
             <path d='M0 0h24v24H0z' fill='none' />
-          </svg> Return to search
+          </svg>
+          <span sx={sx.text}>Return to search</span>
         </Link>
       </BaseStyles>
     )
--- @ndlib/gatsby-theme-marble/src/components/Internal/ReturnToSearch/sx.js
+++ @ndlib/gatsby-theme-marble/src/components/Internal/ReturnToSearch/sx.js
@@ -1,6 +1,15 @@
 module.exports = {
+  link: {
+    textDecoration: 'none',
+  },
   svg: {
     verticalAlign: 'middle',
     color: 'primary',
+  },
+  text: {
+    verticalAlign: 'top',
+    fontSize: '0.8rem',
+    lineHeight: '1.7rem',
+    paddingLeft: '0.2rem',
   },
 }
