7d72068b4082d821a4b238a4ad349837b2102d0a
jmille79 <jmille79@nd.edu>
2020-06-02 10:18:04 -0400

    Updated to proper location

--- @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadMetadata/AboutIIIF/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadMetadata/AboutIIIF/index.js
@@ -5,17 +5,9 @@ const AboutIIIF = () => {
     <div>
       <p>
         <a
-          href='https://iiif.io/community/faq/'
+          href='https://iiif.io/'
           target='_blank'
-          rel='noopener noreferrer'>IIIF</a> is an international image sharing framework. The IIIF manifest URL allows you to compare images across collections or institutions. Learn more about
-        <a
-          href='https://iiif.io/explainers/using_iiif_resources/#mirador'
-          target='_blank'
-          rel='noopener noreferrer'> using IIIF for comparative research</a> or
-        <a
-          href='https://sites.nd.edu/marble/iiif-at-notre-dame-or-the-heart-of-marble/'
-          target='_blank'
-          rel='noopener noreferrer'> how ND is using the technology</a>.
+          rel='noopener noreferrer'>IIIF or the International Image Interoperability Framework</a>, is an open-source image sharing system.
       </p>
     </div>
   )
