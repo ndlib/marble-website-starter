bec7f222a22b85cb4aa7ab582ddf7b6d30b89272
Dan Wolfe <dwolfe2@nd.edu>
2020-06-09 11:18:15 -0400

    Spanish example.

--- site/src/@ndlib/gatsby-theme-marble/i18n/index-example.js
+++ site/src/@ndlib/gatsby-theme-marble/i18n/index-example.js
@@ -1,9 +1,9 @@
 import i18next from 'i18next'
 import config from '@ndlib/gatsby-theme-marble/src/i18n/config'
-import commonTLH from 'assets/i18n/json/common.tlh.json'
+import commonES from 'assets/i18n/json/common.es.json'
 
 i18next.init(config)
-i18next.addResourceBundle('tlh', 'common', commonTLH)
-i18next.languages = ['en', 'tlh']
-i18next.changeLanguage('tlh')
+i18next.addResourceBundle('es', 'common', commonES)
+i18next.languages = ['en', 'es']
+i18next.changeLanguage('es')
 export default i18next
