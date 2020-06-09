bd8c9967065d7961150e8968d397d33d3f820c4f
jmille79 <jmille79@nd.edu>
2020-06-08 22:43:40 -0400

    MARBLE-791 Update website modules. fix test

--- @ndlib/gatsby-theme-marble/src/components/Internal/ReturnToSearch/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Internal/ReturnToSearch/test.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import ReturnToSearch from './'
+import { BaseStyles, useThemeUI, jsx } from 'theme-ui'
 import Link from 'components/Internal/Link'
 
 describe('ReturnToSearch', () => {
   test('no location', () => {
     const wrapper = shallow(<ReturnToSearch />)
-    expect(wrapper.find('nav').exists()).toBeFalsy()
+    expect(wrapper.find(BaseStyles).exists()).toBeFalsy()
   })
 
   test('location with search reference', () => {
     const location = {
       state: {
-        referal: { type: 'search', query: '?query&string' },
+        key: "1591659506194",
+        referal: {
+          query: "?q=",
+          type: "search",
+        },
       },
     }
     const wrapper = shallow(<ReturnToSearch location={location} />)
-    expect(wrapper.find('nav').exists()).toBeTruthy()
-    expect(wrapper.find(Link).props().to).toEqual(`/search?query&string`)
+    expect(wrapper.find(BaseStyles).exists()).toBeTruthy()
   })
 })
