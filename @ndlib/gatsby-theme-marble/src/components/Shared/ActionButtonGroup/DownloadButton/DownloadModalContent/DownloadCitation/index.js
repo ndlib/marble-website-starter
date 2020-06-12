0c765f618f4b528348cb2ae3a86ec0aea5b21735
jmille79 <jmille79@nd.edu>
2020-06-12 14:11:30 -0400

    Suggested changes

--- @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/index.js
@@ -1,43 +1,56 @@
 /** @jsx jsx */
 import PropTypes from 'prop-types'
 import { jsx } from 'theme-ui'
+import { useStaticQuery, graphql } from 'gatsby'
 import typy from 'typy'
+import sx from './sx'
 
 const DownloadCitation = ({ iiifManifest }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+      }
+    `,
+  )
   let biblio = ''
   const metadata = {}
   if (iiifManifest.type === 'Collection') {
-    biblio = iiifManifest.label.en[0] + ', ' + iiifManifest.slug.replace('item/', '') + '. ' +
-      iiifManifest.provider[0].homepage[0].label.en[0] + ', Hesburgh Libraries, University of Notre Dame, South Bend, IN. ' +
-      window.location.href
-  } else {
-    if (typy(iiifManifest, 'provider[0].homepage[0]label.en[0]').isString) {
-      iiifManifest.metadata.map((md) => {
-        metadata[(md.label.en[0].trim())] = md.value.en[0]
-        return metadata
-      })
-      if (iiifManifest.provider[0].homepage[0].label.en[0] === 'Snite Museum of Art') {
-        biblio = metadata.Creator + ', ' + iiifManifest.label.en[0] + ', ' + metadata.Medium + '. ' +
-        'Snite Museum of Art, University of Notre Dame. ' +
-        metadata['Credit Line'] + ', ' + metadata['Accession number'] + '.'
-      }
-      if (iiifManifest.provider[0].homepage[0].label.en[0] === 'Rare Books and Special Collections' | 'University of Notre Dame Archives') {
-        biblio = metadata.Creator + '. ' + iiifManifest.label.en[0] + ', ' + metadata.Date + '. ' +
-        metadata.Collection + '. ' + iiifManifest.provider[0].homepage[0].label.en[0] + ', Hesburgh Libraries, University of Notre Dame, South Bend, IN.' +
-        window.location.href
-      }
-    } else {
-      biblio = 'Unable to render citation'
+    biblio = `${typy(iiifManifest, 'label.en[0]').safeString}, ${typy(iiifManifest, 'slug').safeString.replace('item/', '')}.
+      ${typy(iiifManifest, 'provider[0].homepage[0].label.en[0]').safeString}, Hesburgh Libraries, University of Notre Dame, South Bend, IN.
+      ${typy(site, 'siteMetadata.siteUrl').safeString}/${typy(iiifManifest, 'slug').safeString}`
+  } else if (typy(iiifManifest, 'provider[0].homepage[0]label.en[0]').isString) {
+    iiifManifest.metadata.map((md) => {
+      metadata[(md.label.en[0].trim())] = md.value.en[0]
+      return metadata
+    })
+    if (typy(iiifManifest, 'provider[0].homepage[0].label.en[0]').safeString === 'Snite Museum of Art') {
+      biblio = `${typy(metadata, 'Creator').safeString}, ${typy(iiifManifest, 'label.en[0]').safeString}, ${typy(metadata, 'Medium').safeString}.
+      Snite Museum of Art, University of Notre Dame.
+      ${typy(metadata, 'Credit Line').safeString}, ${typy(metadata, 'Accession number').safeString}.`
+    }
+    if (typy(iiifManifest, 'provider[0].homepage[0].label.en[0]').safeString === 'Rare Books and Special Collections' | 'University of Notre Dame Archives') {
+      biblio = `${typy(metadata, 'Creator').safeString}. ${typy(iiifManifest, 'label.en[0]').safeString}, ${(metadata, 'Date').safeString}.
+      ${typy(metadata, 'Collection').safeString}. ${typy(iiifManifest, 'provider[0].homepage[0].label.en[0]').safeString}, Hesburgh Libraries, University of Notre Dame, South Bend, IN.
+      ${typy(site, 'siteMetadata.siteUrl').safeString}/${typy(iiifManifest, 'slug').safeString}`
     }
   }
-  return (
-    <div sx={{}}>
-      Bibliography Citation, Chicago Style 17th ed. We are making assumptions about
-      the genre of these works and as result the citation may not fit your needs.
-      Please verify for accuracy before including them in your work.
-      <div className='citation'>{biblio}</div>
-    </div>
-  )
+  if (biblio === '') {
+    return null
+  } else {
+    return (
+      <div sx={{}}>
+        Bibliography Citation, Chicago Style 17th ed. We are making assumptions about
+        the genre of these works and as result the citation may not fit your needs.
+        Please verify for accuracy before including them in your work.
+        <div className='citation' sx={sx.citation}>{biblio}</div>
+      </div>
+    )
+  }
 }
 
 DownloadCitation.propTypes = {
---
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/sx.js
@@ -0,0 +1,8 @@
+module.exports = {
+  citation: {
+    marginTop: '1rem',
+    backgroundColor: 'gray.0',
+    padding: '1rem',
+    fontSize: '.9rem',
+  },
+}
