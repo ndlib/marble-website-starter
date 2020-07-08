85002e1fc22fa54d651a4941d1442f7a0b8de685
Jon Hartzler <jon.hartzler@gmail.com>
2020-07-08 17:03:11 -0400

    fix tests

--- @ndlib/gatsby-theme-marble/src/utils/__tests__/test.mapImageMetadata.js
+++ @ndlib/gatsby-theme-marble/src/utils/__tests__/test.mapImageMetadata.js
@@ -5,76 +5,21 @@ let defaultData = []
 describe('mapImageMetadata', () => {
   beforeEach(() => {
     defaultData = {
-      sourceSystem: 'archivesspace',
+      level: 'file',
+      iiifImageUri: 'https://image.server.com/iiif/file',
+      sequence: 0,
       title: 'title',
-      level: 'collection',
-      items: [
-        {
-          title: 'title',
-          level: 'manifest',
-          items: [
-            {
-              level: 'file',
-              iiifImageUri: 'https://image.server.com/iiif/file',
-            },
-          ],
-        },
-        {
-          sourceSystem: 'archivesspace',
-          title: 'title2',
-          level: 'manifest',
-          items: [
-            {
-              level: 'file',
-              iiifImageUri: 'https://image.server.com/iiif/file2',
-            },
-          ],
-        },
-      ],
     }
   })
 
   test('it creates images data out of the file node', () => {
-    const test = [
-      {
-        default: 'https://image.server.com/iiif/file/full/full/0/default.jpg',
-        service: 'https://image.server.com/iiif/file',
-        thumbnail: 'https://image.server.com/iiif/file/full/400,/0/default.jpg',
-        itemId: '',
-        coverImage: '',
-        order: '',
-        label: '',
-        type: '',
-      },
-    ]
-
-    const result = imageMetadata(defaultData.items[0])
-    expect(result).toEqual(test)
-  })
-
-  it('merges all the images from children into a parent', () => {
-    const test = [
-      {
-        default: 'https://image.server.com/iiif/file/full/full/0/default.jpg',
-        service: 'https://image.server.com/iiif/file',
-        thumbnail: 'https://image.server.com/iiif/file/full/400,/0/default.jpg',
-        itemId: '',
-        coverImage: '',
-        order: '',
-        label: '',
-        type: '',
-      },
-      {
-        default: 'https://image.server.com/iiif/file2/full/full/0/default.jpg',
-        service: 'https://image.server.com/iiif/file2',
-        thumbnail: 'https://image.server.com/iiif/file2/full/400,/0/default.jpg',
-        itemId: '',
-        coverImage: '',
-        order: '',
-        label: '',
-        type: '',
-      },
-    ]
+    const test = {
+      default: 'https://image.server.com/iiif/file/full/full/0/default.jpg',
+      service: 'https://image.server.com/iiif/file',
+      thumbnail: 'https://image.server.com/iiif/file/full/400,/0/default.jpg',
+      sequence: 0,
+      title: 'title',
+    }
 
     const result = imageMetadata(defaultData)
     expect(result).toEqual(test)
