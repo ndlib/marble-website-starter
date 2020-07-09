ac9813c3be0b82449e5da75f80bb4b543a017b5b
Dan Wolfe <dwolfe2@nd.edu>
2020-07-09 11:04:48 -0400

    cleanup unused

--- @ndlib/gatsby-theme-marble/src/components/Shared/ChildManifests/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ChildManifests/index.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types'
 import typy from 'typy'
 import DisplayViewToggle from 'components/Internal/DisplayViewToggle'
 import ManifestCard from '../ManifestCard'
-import getLanguage from 'utils/getLanguage'
 
-export const ChildManifests = ({ marbleItem, displayReducer }) => {
+export const ChildManifests = ({ marbleItem }) => {
   if (!marbleItem || !typy(marbleItem, 'childrenMarbleItem').isArray) {
     return null
   }
-  const lang = getLanguage()
   return (
     <>
       <h2 className='accessibilityOnly'>Related Items</h2>
@@ -35,7 +33,6 @@ export const ChildManifests = ({ marbleItem, displayReducer }) => {
 
 ChildManifests.propTypes = {
   marbleItem: PropTypes.object.isRequired,
-  displayReducer: PropTypes.object,
 }
 
 export default ChildManifests
