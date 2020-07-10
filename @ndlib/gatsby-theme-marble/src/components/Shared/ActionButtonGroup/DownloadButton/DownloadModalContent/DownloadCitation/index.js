638150c00645fc09d9b2eb96c16fb285c7550a1f
jmille79 <jmille79@nd.edu>
2020-07-10 11:18:49 -0400

    Shift how citation information is generated

--- @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/index.js
@@ -1,5 +1,12 @@
-const DownloadCitation = ({ iiifManifest }) => {
-  return null
+import React from 'react'
+import PropTypes from 'prop-types'
+const DownloadCitation = ({ marbleItem }) => {
+  return marbleItem.citation ? <div>{marbleItem.citation}</div> : null
 }
-
 export default DownloadCitation
+
+DownloadCitation.propTypes = {
+  marbleItem: {
+    citation: PropTypes.string,
+  },
+}
---
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import DownloadCitation from './'
+import { useStaticQuery } from 'gatsby'
+
+describe('DownloadCitation', () => {
+  test('NoCitation', () => {
+    const manifest = {
+      slug: 'item/slug',
+    }
+    useStaticQuery.mockImplementation(() => {
+      return manifest
+    })
+    const wrapper = shallow(<DownloadCitation marbleItem={manifest} />)
+    expect(wrapper).toEqual({})
+  })
+  test('Citation', () => {
+    const manifest = {
+      citation: 'The Citation',
+    }
+    const wrapper = shallow(<DownloadCitation marbleItem={manifest} />)
+    expect(wrapper.find('div').text()).toEqual('The Citation')
+  })
+})
