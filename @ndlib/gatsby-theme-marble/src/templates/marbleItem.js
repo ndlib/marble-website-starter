0d2556db8d75cb855a5245e3c7b79f617b01e361
Dan Wolfe <dwolfe2@nd.edu>
2020-07-17 10:07:33 -0400

    fix props, add debug check

--- @ndlib/gatsby-theme-marble/src/templates/marbleItem.js
+++ @ndlib/gatsby-theme-marble/src/templates/marbleItem.js
@@ -2,8 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import MarbleItem from 'components/Pages/MarbleItem'
+import queryString from 'query-string'
 
 export const MarbleItemPage = ({ data, location }) => {
+  // use ?debug=true to render graphQL data at end of page
+  const { debug } = queryString.parse(location.search)
   return (
     <>
       <MarbleItem
@@ -11,7 +14,9 @@ export const MarbleItemPage = ({ data, location }) => {
         location={location}
       />
       {
-        // <pre>{JSON.stringify(data, null, 2)}</pre>
+        debug ? (
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        ) : null
       }
     </>
   )
