f2d4764f1323760d39ea996d85ecf29ae4a2f900
Dan Wolfe <dwolfe2@nd.edu>
2020-07-17 09:09:19 -0400

    add scripts to build process

--- scripts/gatsby-source-iiif/generate.sh
+++ scripts/gatsby-source-iiif/generate.sh
@@ -4,4 +4,6 @@ site="../../site"
 # node getManifests.js ${site}
 # node generateMD.js ${site}
 node getSchema.js ${site}
+node getImageInfo.js ${site}
+node downloadImages.js ${site}
 node indexSearch.js ${site}
