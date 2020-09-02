98f548d94e47fb0cfbcbb98a924741ed94e58591
jmille79 <jmille79@nd.edu>
2020-09-02 11:27:17 -0400

    typo fix

--- @ndlib/gatsby-theme-marble/src/components/Shared/LoginArea/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/LoginArea/test.js
@@ -6,7 +6,6 @@ import OktaLogin from './OktaLogin'
 import * as auth from 'utils/auth'
 import i18n from '@ndlib/gatsby-theme-marble/src/i18n/i18nextForTest'
 
-
 describe('LoginArea', () => {
   test('it renders the log out button when the user is logged in', () => {
     jest.spyOn(auth, 'isLoggedIn').mockImplementation(() => true)
