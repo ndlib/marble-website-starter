3cb27053326f13d88fbe36355bb1290b86b58975
jmille79 <jmille79@nd.edu>
2020-09-23 10:41:30 -0400

    MARBLE-1128 Address the skipped tests.

--- @ndlib/gatsby-theme-marble/src/components/App/Pages/User/NoUser/test.js
+++ @ndlib/gatsby-theme-marble/src/components/App/Pages/User/NoUser/test.js
@@ -1,5 +1,14 @@
-// import React from 'react'
-// import { shallow } from 'enzyme'
-// import NoUser from './'
+import React from 'react'
+import { shallow } from 'enzyme'
+import { act } from 'react-dom/test-utils'
+import { Trans } from 'react-i18next'
+import NoUser from './'
 
-test.skip('NoUser', () => {})
+test('NoUser renders', () => {
+  const user = 'Anonymous'
+  let wrapper
+  act(() => {
+    wrapper = shallow(<NoUser userName={user} />)
+  })
+  expect(wrapper.find(Trans).exists()).toBeTruthy()
+})
