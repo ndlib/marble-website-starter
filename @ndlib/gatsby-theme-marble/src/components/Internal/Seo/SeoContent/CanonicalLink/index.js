8431396e52e76fb9a7abe50c16ffd2a53af1cf57
jmille79 <jmille79@nd.edu>
2020-09-25 10:36:04 -0400

    proper changes

--- @ndlib/gatsby-theme-marble/src/components/Internal/Seo/SeoContent/CanonicalLink/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Internal/Seo/SeoContent/CanonicalLink/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import favicon from 'assets/icons/favicon.ico'
 const CanonicalLink = ({ base, pathname }) => {
   if (!pathname) {
     return null
   }
   return (
     <Helmet>
-      <link rel='icon' href={favicon} />
       <link rel='canonical' href={`${base}${pathname}`} />
     </Helmet>
   )
