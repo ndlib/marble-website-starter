ee351a6ecc6734915a75608b2ee8258df5cfa9d3
Jon Hartzler <jon.hartzler@gmail.com>
2020-09-28 15:19:48 -0400

    expand -- subjects for keywords

--- scripts/gatsby-source-iiif/src/getKeywordsFromSubjects/index.js
+++ scripts/gatsby-source-iiif/src/getKeywordsFromSubjects/index.js
@@ -8,7 +8,31 @@ module.exports = (manifest) => {
     manifest.subjects = JSON.parse(manifest.subjects.replace(/'/g, '"'))
   }
 
-  return manifest.subjects.map(m => {
-    return m.term
+  return [...new Set([].concat.apply([], manifest.subjects.map(m => {
+    return splitTerms(m.term).concat(addVariants(m)).concat(addBroaderTerms(m))
+  })))].map((t) => {
+    return t.trim()
+  })
+}
+
+const splitTerms = (term) => {
+  return term.split('--')
+}
+
+const addVariants = (subject) => {
+  if (!subject.variants) {
+    return []
+  }
+
+  return subject.variants
+}
+
+const addBroaderTerms = (subject) => {
+  if (!subject.broaderTerms) {
+    return []
+  }
+
+  return subject.broaderTerms.map((bt) => {
+    return bt.term
   })
 }
