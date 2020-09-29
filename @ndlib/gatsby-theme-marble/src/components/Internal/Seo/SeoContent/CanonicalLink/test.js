e8407611037e9f9b87956a951e159fc96b339a62
jmille79 <38439231+jmille79@users.noreply.github.com>
2020-09-29 10:00:04 -0400

    final fix (#353)

--- @ndlib/gatsby-theme-marble/src/components/Internal/Seo/SeoContent/CanonicalLink/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Internal/Seo/SeoContent/CanonicalLink/test.js
@@ -6,13 +6,13 @@ import CanonicalLink from './'
 describe('CanonicalLink', () => {
   test('null pathname', () => {
     const wrapper = shallow(<CanonicalLink base={'http://example'} />)
-    expect(wrapper.find('link').exists()).toBeFalsy()
+    expect(wrapper.find('link').at(1).exists()).toBeFalsy()
   })
 
   test('valid pathname', () => {
     const wrapper = shallow(<CanonicalLink base={'http://example'} pathname={'/test/path'} />)
     expect(wrapper.find(Helmet).exists()).toBeTruthy()
-    expect(wrapper.find('link').props().rel).toEqual('canonical')
-    expect(wrapper.find('link').props().href).toEqual('http://example/test/path')
+    expect(wrapper.find('link').at(1).props().rel).toEqual('canonical')
+    expect(wrapper.find('link').at(1).props().href).toEqual('http://example/test/path')
   })
 })
