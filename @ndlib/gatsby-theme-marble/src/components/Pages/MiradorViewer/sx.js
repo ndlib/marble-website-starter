2080c74bb7d3a92e49b9a485a7060aa122fecc4b
jmille79 <38439231+jmille79@users.noreply.github.com>
2020-10-05 18:44:41 -0400

    MARBLE-1124 Fix Mirador Viewer position (#360)

--- @ndlib/gatsby-theme-marble/src/components/Pages/MiradorViewer/sx.js
+++ @ndlib/gatsby-theme-marble/src/components/Pages/MiradorViewer/sx.js
@@ -1,7 +1,7 @@
 module.exports = {
   div: {
     height: 'calc(100vh - 70px)',
-    margin: ['-1rem -1.5rem', '-1rem -2.5rem', '-1rem -2.5rem'],
+    margin: ['-.65rem -1.5rem', '-.65rem -2.5rem', '-.65rem -2.5rem'],
     position: 'relative',
     zIndex: '0',
   },
