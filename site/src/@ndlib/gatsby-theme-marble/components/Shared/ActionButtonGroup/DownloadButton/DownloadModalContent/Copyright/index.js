6da98e3ae3249a54db2ddaa3c4955e2b04d9a63e
Jon Hartzler <jon.hartzler@gmail.com>
2020-10-13 10:17:12 -0400

    change download modal

--- site/src/@ndlib/gatsby-theme-marble/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/Copyright/index.js
+++ site/src/@ndlib/gatsby-theme-marble/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/Copyright/index.js
@@ -3,11 +3,12 @@ import { BaseStyles, jsx } from 'theme-ui'
 import Link from '@ndlib/gatsby-theme-marble/src/components/Internal/Link'
 import sx from '@ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/Copyright/sx'
 import { useTranslation } from 'react-i18next'
+
 export const Copyright = () => {
   const { t } = useTranslation()
   return (
     <BaseStyles>
-      <div sx={sx.wrapper}>© {new Date().getFullYear()}{t("text:actionGroup.copyright")}<Link to='help/copyright-and-permissions'>Copyright and permissions</Link>.</div>
+      <div sx={sx.wrapper}><Link to='help/copyright-and-permissions'>{t("text:actionGroup.copyright-link")}</Link>.</div>
     </BaseStyles>
   )
 }
