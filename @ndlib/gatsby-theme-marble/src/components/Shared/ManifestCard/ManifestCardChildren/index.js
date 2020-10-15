98e0ce118f24f29262477e2c01e5a1ec5f91585c
Dan Wolfe <dwolfe2@nd.edu>
2020-10-15 16:51:07 -0400

    code climate

--- @ndlib/gatsby-theme-marble/src/components/Shared/ManifestCard/ManifestCardChildren/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ManifestCard/ManifestCardChildren/index.js
@@ -3,28 +3,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { jsx } from 'theme-ui'
-import typy from 'typy'
-import sx from '../sx'
+import ChildField from './ChildField'
 
 // eslint-disable-next-line complexity
 export const ManifestCardChildren = ({ parentProps, date, creator }) => {
   return (
     <>
-      {
-        creator ? (
-          <p
-            sx={sx.lineStyle}
-            dangerouslySetInnerHTML={{ __html: typy(creator).isArray ? creator.join('<br/>') : creator }}
-          />
-        ) : null
-      }{
-        date ? (
-          <p
-            sx={sx.lineStyle}
-            dangerouslySetInnerHTML={{ __html: typy(date).isArray ? date.join('<br/>') : date }}
-          />
-        ) : null
-      }
+      <ChildField field={creator} />
+      <ChildField field={date} />
       {parentProps.children ? parentProps.children : null}
     </>
   )
