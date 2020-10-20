8d21c914cd8a32e65ff3d58d652ef6f968eb0b76
jmille79 <38439231+jmille79@users.noreply.github.com>
2020-10-20 09:20:58 -0400

    MARBLE-1197 Fix footer sx (#373)
    * MARBLE-1197 Fix footer sx

--- site/src/@ndlib/gatsby-theme-marble/components/Layout/PageWrapper/Footer/index.js
+++ site/src/@ndlib/gatsby-theme-marble/components/Layout/PageWrapper/Footer/index.js
@@ -10,70 +10,65 @@ import libraryLogo from 'assets/images/library.logo.png'
 import ndLogo from 'assets/images/ND_mark_white_M.png'
 import sx from './sx'
 export const Footer = () => {
-  const date = new Date()
   return (
     <ThemeFooter>
       <div sx={sx.flexWrapper}>
-        <div sx={sx.footerContacts}>
-          <div sx={sx.footerColumn}>
-            <Link to='/'>
-              <img
-                src={marbleLogo}
-                sx={sx.image}
-                alt='MARBLE: Museums, Archives, Rare Books & Libraries Exploration'
-              />
-            </Link>
-            <a
-              href='https://sniteartmuseum.nd.edu/'
-              sx={sx.imageWrapper}
-            >
-              <img
-                src={sniteLogo}
-                sx={sx.image}
-                alt='Snite Art Museum'
-              />
-            </a>
-            <p className='contact-info'>
-              <div className='address' property='address' typeof='PostalAddress'>
-                <span property='streetAddress'>100 Moose Krause Circle</span><br />
-                <span property='addressLocality'>Notre Dame</span>, <span property='addressRegion'>IN</span> <span property='postalCode'>46556</span> <span property='addressCountry'>USA</span>
-              </div>
-              <div className='footer-phone' property='telephone' content='+1 574-631-5466'>Phone <a href='tel:574-631-5466'>(574) 631–5466</a></div>
-              <div className='footer-email' property='email'><a rel='noopener' href='mailto:sniteart@nd.edu'>sniteart@nd.edu</a></div>
-            </p>
-            <a
-              href='https://library.nd.edu'
-              sx={sx.imageWrapper}
-            >
-              <img
-                src={libraryLogo}
-                sx={sx.image}
-                alt='Hesburgh Library'
-              />
-            </a>
-            <p className='contact-info'>
-              <div className='address' property='address' typeof='PostalAddress'>
-                <span property='streetAddress'>284 Hesburgh Library</span><br />
-                <span property='addressLocality'>Notre Dame</span>, <span property='addressRegion'>IN</span> <span property='postalCode'>46556</span> <span property='addressCountry'>USA</span>
-              </div>
-              <div className='footer-phone' property='telephone' content='+1 574-631-6679'>Phone <a href='tel:574-631-6679'>(574) 631–6679</a></div>
-              <div className='footer-email' property='email'><a rel='noopener' href='mailto:asklib@nd.edu'>asklib@nd.edu</a></div>
-            </p>
+        <div sx={sx.footerLeftColumn}>
+          <Link to='/'>
+            <img
+              src={marbleLogo}
+              sx={sx.image}
+              alt='MARBLE: Museums, Archives, Rare Books & Libraries Exploration'
+            />
+          </Link>
+          <a
+            href='https://sniteartmuseum.nd.edu/'
+            sx={sx.imageWrapper}
+          >
+            <img
+              src={sniteLogo}
+              sx={sx.image}
+              alt='Snite Art Museum'
+            />
+          </a>
+          <div className='contact-info'>
+            <div className='address' property='address' typeof='PostalAddress'>
+              <span property='streetAddress'>100 Moose Krause Circle</span><br />
+              <span property='addressLocality'>Notre Dame</span>, <span property='addressRegion'>IN</span> <span property='postalCode'>46556</span> <span property='addressCountry'>USA</span>
+            </div>
+            <div className='footer-phone' property='telephone' content='+1 574-631-5466'>Phone <a href='tel:574-631-5466'>(574) 631–5466</a></div>
+            <div className='footer-email' property='email'><a rel='noopener' href='mailto:sniteart@nd.edu'>sniteart@nd.edu</a></div>
+          </div>
+          <a
+            href='https://library.nd.edu'
+            sx={sx.imageWrapper}
+          >
+            <img
+              src={libraryLogo}
+              sx={sx.image}
+              alt='Hesburgh Library'
+            />
+          </a>
+          <div className='contact-info'>
+            <div className='address' property='address' typeof='PostalAddress'>
+              <span property='streetAddress'>284 Hesburgh Library</span><br />
+              <span property='addressLocality'>Notre Dame</span>, <span property='addressRegion'>IN</span> <span property='postalCode'>46556</span> <span property='addressCountry'>USA</span>
+            </div>
+            <div className='footer-phone' property='telephone' content='+1 574-631-6679'>Phone <a href='tel:574-631-6679'>(574) 631–6679</a></div>
+            <div className='footer-email' property='email'><a rel='noopener' href='mailto:asklib@nd.edu'>asklib@nd.edu</a></div>
           </div>
           <div sx={sx.copyright} className='copyright'><a href='https://www.nd.edu/copyright/'>&copy; 2020</a> <a href='https://www.nd.edu'>University of Notre Dame</a></div>
         </div>
-      </div>
-      <div sx={sx.footerColumn}>
-        <div className='footer-parent' property='parentOrganization' typeof='CollegeOrUniversity' resource='#parentorg'>
-          <meta property='name' content='University of Notre Dame' />
-          <a href='https://www.nd.edu/' className='mark-footer' property='url logo' typeof='ImageObject' aria-label='University of Notre Dame'>
-            <img src={ndLogo} width='250' height='60' alt='University of Notre Dame' property='url' />
-          </a>
-        </div>
-        <div sx={sx.textWrapper}>
-          <div sx={sx.menuWrapper}>
-            <Menu menu='footer' />
-            <a href='https://www.nd.edu/about/accessibility/' aria-label='Notre Dame Accessibility Information'>Accessibility</a>
+        <div sx={sx.footerRightColumn}>
+          <div sx={sx.textWrapper}>
+            <meta property='name' content='University of Notre Dame' />
+            <a href='https://www.nd.edu/' className='mark-footer' property='url logo' typeof='ImageObject' aria-label='University of Notre Dame'>
+              <img src={ndLogo} width='250' height='60' alt='University of Notre Dame' property='url' />
+            </a>
+            <div sx={sx.menuWrapper}>
+              <Menu menu='footer' />
+              <a href='https://www.nd.edu/about/accessibility/' aria-label='Notre Dame Accessibility Information'>Accessibility</a>
+            </div>
           </div>
         </div>
       </div>
