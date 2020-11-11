ea6bbb8bcdf8cf3ecc557dbc707e4ae5ee08d84c
Dan Wolfe <dwolfe2@nd.edu>
2020-11-11 11:34:48 -0500

    Create new component instead of overriding ReturnToSearch functionality.

--- @ndlib/gatsby-theme-marble/src/components/Layout/PageContent/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Layout/PageContent/index.js
@@ -3,24 +3,17 @@ import PropTypes from 'prop-types'
 import { Main, BaseStyles } from 'theme-ui'
 import UserAnnotation from 'components/Internal/UserAnnotation'
 import ReturnToSearch from 'components/Internal/ReturnToSearch'
+import ParentLink from 'components/Internal/ParentLink'
 
 const PageContent = ({
   title,
   children,
   location,
 }) => {
-  location.state = children[0].props.data.marbleItem.marbleParent ? (
-    {
-      referal: {
-        parentName: children[0].props.data.marbleItem.marbleParent.title,
-        type: 'item',
-        backLink: children[0].props.data.marbleItem.marbleParent.slug,
-      },
-    }
-  ) : null
   return (
     <Main id='mainContent'>
       <ReturnToSearch location={location} />
+      <ParentLink>{children}</ParentLink>
       {title ? <BaseStyles><h1>{title}</h1></BaseStyles> : null}
       <UserAnnotation location={location} />
       <article>{children}</article>
