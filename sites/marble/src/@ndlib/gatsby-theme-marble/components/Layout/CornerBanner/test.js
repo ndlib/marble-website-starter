c121677df83ae7a2d1de17d0a40774fa1fd43396
Dan Wolfe <dwolfe2@nd.edu>
2021-01-15 11:35:22 -0500

    Remove /sites/marble

--- sites/marble/src/@ndlib/gatsby-theme-marble/components/Layout/CornerBanner/test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import CornerBanner from './'
-
-test('CornerBanner', () => {
-  const wrapper = shallow(<CornerBanner />)
-  expect(wrapper.find('.versionText').text()).toEqual('This is a beta preview of the MARBLE website.')
-})
