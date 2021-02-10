a5debd50d05b3b790875535768de6d206f529347
dan <dwolfe2@nd.edu>
2021-02-10 09:33:09 -0500

    update starter

--- @ndlib/gatsby-starter-marble/gatsby-config.js
+++ @ndlib/gatsby-starter-marble/gatsby-config.js
@@ -2,6 +2,14 @@ const configuration = require('./content/configuration')
 module.exports = {
   siteMetadata: configuration.siteMetadata,
   plugins: [
+    'gatsby-transformer-marbleitem',
     '@ndlib/gatsby-theme-marble',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'standard',
+        path: 'content/json/standard',
+      },
+    },
   ],
 }
