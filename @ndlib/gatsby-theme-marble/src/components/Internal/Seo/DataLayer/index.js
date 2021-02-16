51d80f5e4b2c0212be2bb99735195c1492de1cd0
Paul Weikel <pweikel@nd.edu>
2021-02-16 11:19:45 -0500

    added DataLayer
    added DataLayer class component that pushes to window on componentDidMount

---
+++ @ndlib/gatsby-theme-marble/src/components/Internal/Seo/DataLayer/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class DataLayer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <></>
+  }
+  componentDidMount() {
+    const { title, author, description, pathname, data } = this.props
+    {
+      window.dataLayer = window.dataLayer || []
+      window.dataLayer.push({
+          title: title,
+          author: author,
+          description: description,
+          location: pathname,
+          data: data,
+      })
+    }
+  }
+}
+
+DataLayer.propTypes = {
+  data: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  author: PropTypes.string,
+}
