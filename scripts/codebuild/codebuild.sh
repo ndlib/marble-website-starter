9fee604d560a6bffc2d6ba8ff16ce80bbc38de2a
Jon Hartzler <jon.hartzler@gmail.com>
2021-02-17 16:29:55 -0500

    remove test code

--- scripts/codebuild/codebuild.sh
+++ scripts/codebuild/codebuild.sh
@@ -80,12 +80,6 @@ if ((failures != 0)); then
 fi
 
 echo "${magenta}----- Build -------${reset}"
-pwd
-ls
-ls sites
-echo ${SUBMOD_DIR}
-ls ${SUBMOD_DIR}
-
 yarn workspace ${WORKSPACE_NAME} build
 
 
