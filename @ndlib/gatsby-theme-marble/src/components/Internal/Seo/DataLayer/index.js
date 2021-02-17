4c4fc7bd8c4c7cc6e2926cead7ed7ac4e70a9804
Paul Weikel <pweikel@nd.edu>
2021-02-17 15:27:21 -0500

    updated dataLayer with event

--- @ndlib/gatsby-theme-marble/src/components/Internal/Seo/DataLayer/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Internal/Seo/DataLayer/index.js
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types'
 
 export default class DataLayer extends React.Component {
   render () {
-    return <></>
+    return
   }
   componentDidMount () {
     const { title, author, description, location, data } = this.props
     window.dataLayer = window.dataLayer || []
     window.dataLayer.push({
-      title: title,
-      author: author,
-      description: description,
-      pathname: location.pathname,
-      data: data,
+      event: 'pageView',
+        category: 'Page View',
+        action: 'Page Refresh',
+        label: 'PageView',
+        title: title,
+        author: author,
+        description: description,
+        pathname: location.pathname,
+        data: data,
     })
   }
 }
