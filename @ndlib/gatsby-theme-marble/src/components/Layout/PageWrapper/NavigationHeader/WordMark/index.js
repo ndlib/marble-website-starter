33ea64b52388e324e90facc40b9677ae3fe192e0
Jon Hartzler <jon.hartzler@gmail.com>
2021-02-17 13:06:59 -0500

    fix code climate

--- @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/WordMark/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader/WordMark/index.js
@@ -1,31 +1,17 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import wordMark from 'assets/logos/dept-nd-white.svg'
+import sx from './sx'
 
 const WordMark = () => {
-  const background = {
-    backgroundColor: '#0C2340',
-    display: ['block', 'none', 'none'],
-    height: '48px',
-    textAlign: 'center',
-  }
-  const link = {
-    backgroundImage: `url(${wordMark})`,
-    backgroundPositionY: 'center',
-    backgroundRepeat: 'no-repeat',
-    display: 'block',
-    lineHeight: '48px',
-    margin: '0 auto',
-    textIndent: '-9999px',
-    overflow: 'hidden',
-    width: '301px',
-  }
+  sx.link.backgroundImage = `url(${wordMark})`
+
   return (
-    <div sx={background}>
+    <div sx={sx.background}>
       <a
         href='https://nd.edu'
         className='mobile'
-        sx={link}
+        sx={sx.link}
       >University <i>of</i> Notre Dame
       </a>
     </div>
