2beaeefbb675ade86572d745cba355a5b5f1538f
Jon Hartzler <jon.hartzler@gmail.com>
2021-02-17 15:09:51 -0500

    fixes for build

--- @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import PageWrapper from './'
 import SkipToMain from './SkipToMain'
-import NavigationHeader from './NavigationHeader'
 import Footer from './Footer'
 
 test('PageWrapper', () => {
@@ -10,7 +9,6 @@ test('PageWrapper', () => {
   const location = { my: 'location' }
   const wrapper = shallow(<PageWrapper location={location}>{children}</PageWrapper>)
   expect(wrapper.find(SkipToMain).exists()).toBeTruthy()
-  expect(wrapper.find(NavigationHeader).props().location).toEqual({ my: 'location' })
   expect(wrapper.find('.kids').text()).toEqual('Stuff Goes Here')
   expect(wrapper.find(Footer).exists()).toBeTruthy()
 })
