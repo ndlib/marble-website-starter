400722f921ce884b5bbf16a2d0553659aef94363
jmille79 <jmille79@nd.edu>
2021-03-04 13:59:48 -0500

    fix discrepancies

--- @ndlib/gatsby-theme-marble/gatsby-config.js
+++ @ndlib/gatsby-theme-marble/gatsby-config.js
@@ -23,9 +23,6 @@ module.exports = ({
         templates: path.join(__dirname, 'src/templates'),
         utils: path.join(__dirname, 'src/utils'),
       },
-      flags: {
-        DEV_SSR: true
-      },
     },
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-sitemap',
