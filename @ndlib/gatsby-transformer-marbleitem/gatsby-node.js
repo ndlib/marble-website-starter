eb7bbc1138a831022217270478745e717452f5a8
dan <dwolfe2@nd.edu>
2021-03-11 14:52:07 -0500

    fix build hang

--- @ndlib/gatsby-transformer-marbleitem/gatsby-node.js
+++ @ndlib/gatsby-transformer-marbleitem/gatsby-node.js
@@ -2,14 +2,6 @@ const pruneEmptyLeaves = require('./src/pruneEmptyLeaves')
 const { crawlStandardJson } = require('./src/crawlStandardJson')
 
 exports.sourceNodes = async (gatsbyInternal, pluginOptions) => {
-  makeMarbleNodes(gatsbyInternal, pluginOptions)
-}
-
-exports.onCreateNode = async (gatsbyInternal, pluginOptions) => {
-  makeMarbleNodes(gatsbyInternal, pluginOptions)
-}
-
-const makeMarbleNodes = async (gatsbyInternal, pluginOptions) => {
   const { getNodesByType } = gatsbyInternal
   const standardJsonNodes = getNodesByType('StandardJson')
   const appsyncStandardNodes = getNodesByType('AppSyncStandard')
@@ -21,4 +13,15 @@ const makeMarbleNodes = async (gatsbyInternal, pluginOptions) => {
     }
     crawlStandardJson(node, null, null, gatsbyInternal)
   })
+}
+
+exports.onCreateNode = async (gatsbyInternal, pluginOptions) => {
+  const { node } = gatsbyInternal
+
+  if (node.internal.type === 'StandardJson' || node.internal.type === 'AppSyncStandard') {
+    if (!pluginOptions.skipMetadataPrune) {
+      pruneEmptyLeaves(node)
+    }
+    crawlStandardJson(node, null, null, gatsbyInternal)
+  }
 }
