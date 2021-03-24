d7ba8025a4f64e376075ee79990cbea8c4c1f78d
Paul Weikel <pweikel@nd.edu>
2021-03-24 12:23:31 -0400

    added WebsiteJsonLd component

---
+++ @ndlib/gatsby-theme-marble/src/components/Shared/Seo/SeoContent/WebsiteJsonLd/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { JsonLd } from 'gatsby-plugin-next-seo'
+
+export const WebsiteJsonLd = ({ pathname, siteUrl }) => {
+  if (pathname === '/')
+  console.log(pathname)
+  return (
+    <>
+      <JsonLd
+        context='https://schema.org'
+        type='Website'
+        url={siteUrl}
+        potentialAction={{
+          type: 'SearchAction',
+          target: `${siteUrl}/search?q={search_term_string}`,
+          queryInput: 'required name=search_term_string',
+        }}
+      />
+    </>
+  )
+}
+
+WebsiteJsonLd.propTypes = { 
+  pathname: PropTypes.string,
+  siteUrl: PropTypes.string.isRequired,
+}
+
+export default WebsiteJsonLd
