e9c26d7de14107f284302af8e9a0170e329bd078
Paul Weikel <pweikel@nd.edu>
2021-03-25 12:16:41 -0400

    removed log

--- @ndlib/gatsby-theme-marble/src/components/Shared/Seo/SeoContent/WebsiteJsonLd/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/Seo/SeoContent/WebsiteJsonLd/test.js
@@ -5,6 +5,5 @@ import { JsonLd } from 'gatsby-plugin-next-seo'
 
 test('Website JSON LD schema on homepage', () => {
   const wrapper = shallow(<WebsiteJsonLd pathname='/' siteUrl='https://marble.nd.edu' />)
-  console.log(wrapper.debug())
   expect(wrapper.find(JsonLd).props().url).toEqual('https://marble.nd.edu')
 })
