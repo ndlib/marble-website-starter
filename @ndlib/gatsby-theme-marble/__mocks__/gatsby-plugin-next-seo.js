927175b5297502b6e93a19e9ca14e93f5687fbd3
Paul Weikel <pweikel@nd.edu>
2021-03-26 11:28:29 -0400

    format changes

--- @ndlib/gatsby-theme-marble/__mocks__/gatsby-plugin-next-seo.js
+++ @ndlib/gatsby-theme-marble/__mocks__/gatsby-plugin-next-seo.js
@@ -1,9 +1,8 @@
-const React = require('react')
 const plugin = jest.requireActual('gatsby-plugin-next-seo')
 
 module.exports = {
   ...plugin,
   JsonLd: jest.fn().mockImplementation(
     () => null
-  )
+  ),
 }
