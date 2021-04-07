45325cfcb6c3f0451462d9ac2e3399134ed4c8d7
Jon Hartzler <jon.hartzler@gmail.com>
2021-04-07 10:08:48 -0400

    save new env commands

--- scripts/codebuild/codebuild.sh
+++ scripts/codebuild/codebuild.sh
@@ -39,7 +39,10 @@ if [[ -z "$PARAM_CONFIG_PATH" ]]; then
     echo "export PARAM_CONFIG_PATH=/all/static-host/<stackname>/"
     exit 2
 fi
-echo ${PARAM_CONFIG_PATH}
+
+if [ $BUILD_ENVIRONMENT == "test" ]; then
+  cp .env.production-test .env.production
+fi
 
 echo "${magenta}----- INSTALLATIONS -------${reset}"
 # install yarn
@@ -60,11 +63,15 @@ pushd scripts/gatsby-source-iiif/
 
   echo "${magenta}----- Build ENV Config -------${reset}"
   # add the keys at /all/static/hostname to the env
-  node setupEnv.js ${PARAM_CONFIG_PATH} > './ssm-params.txt' --unhandled-rejections=strict
-  source ./ssm-params.txt
+  # node setupEnv.js ${PARAM_CONFIG_PATH} > './ssm-params.txt' --unhandled-rejections=strict
+
+  # put the env sourced vars in the environment file.
+  echo "S3_DEST_BUCKET='${S3_DEST_BUCKET}'" >> $ENV_FILE
+  echo "GRAPHQL_KEY_BASE='${GRAPHQL_KEY_BASE}'" >> $ENV_FILE
+  echo "SEARCH_URL='${SEARCH_URL}'" >> $ENV_FILE
 
   # add the app sync keys to the env
-  node setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} > './ssm-params.txt' --unhandled-rejections=strict
+  node setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> ${ENV_FILE} --unhandled-rejections=strict
   source ./ssm-params.txt
 
   echo "${magenta}----- Get Metadata -------${reset}"
---
+++ scripts/setup-development.sh
@@ -0,0 +1,13 @@
+SITE=$1
+BASE_DIR="${PWD}"
+SITE_DIR="${BASE_DIR}/sites/${SITE}"
+ENV_FILE="${SITE_DIR}/.env.development"
+EXAMPLE_FILE="${SITE_DIR}/.env.development-example"
+
+cp ${EXAMPLE_FILE} ${ENV_FILE}
+
+GRAPHQL_API_KEY_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
+# add the app sync keys to the env
+node ./scripts/codebuild/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> ${ENV_FILE} --unhandled-rejections=strict
+
+echo "Reset .env.development and double check your keys."
