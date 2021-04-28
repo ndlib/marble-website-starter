f24e06d2386a42e281b69fe6673990acedcb30b2
Paul Weikel <pweikel@nd.edu>
2021-04-28 16:42:38 -0400

    updated unit test

--- @ndlib/gatsby-theme-marble/src/components/Shared/Seo/SeoContent/SchemaJsonLd/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/Seo/SeoContent/SchemaJsonLd/test.js
@@ -7,13 +7,28 @@ describe('SchemaJsonLd component', () => {
   it('renders props into values', () => {
     const tree = renderer
       .create(<SchemaJsonLd
-        title={'Fake Title'}
-        description={'This is a fake description'}
+        title={'1984'}
         image={'http://fake.io/image'}
-        author={'Fake Author'}
-        creditText={'Fake Creditor'}
+        author={'George Orwell'}
+        creditText={'The World atlarge'}
         dimensions={'2 x 4'}
-        thumbnail={'http://fake.io/thumbnail'} />)
+        thumbnail={'http://fake.io/thumbnail'}
+        url={'http://object.io/page'}
+        relatedLocation={'London'}
+        date={'Present'}
+        description={'Dystopian nightmare'}
+        language={'Newspeak'}
+        keywords={'totalitarian','society','thoughtcrime','doublespeak'}
+        physicalCharacteristic={'8in x 6in'}
+        classification={'Book'}
+        materialType={'Paper'}
+        creditText={'BBC'}
+        copyrightStatus={'Public Domain'}
+        publisher={'BBC'}
+        identifier={'2+2=5'}
+        acquisition={'a gift from me'}
+        campusLocation={'library'}
+        />)
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
