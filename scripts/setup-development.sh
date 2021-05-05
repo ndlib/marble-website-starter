59763bfed1d2ec4e4ce63e89268fe0a448d83f7f
dan <danthewolfe@users.noreply.github.com>
2021-05-05 14:24:45 -0400

    MARBLE-1642 Fix Github Actions (#530)
    * MARBLE-1642 Attempt to fix Github Actions
    
    * some more changes
    
    * Update marble
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * move s3_dest_bucket stub config here

--- scripts/setup-development.sh
+++ scripts/setup-development.sh
@@ -8,6 +8,6 @@ cp ${EXAMPLE_FILE} ${ENV_FILE}
 
 GRAPHQL_API_KEY_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
 # add the app sync keys to the env
-node ./scripts/gatsby-source-iiif/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> ${ENV_FILE} --unhandled-rejections=strict
+node ./scripts/environment-configuration/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> ${ENV_FILE} --unhandled-rejections=strict
 
 echo "Reset .env.development and double check your keys."
