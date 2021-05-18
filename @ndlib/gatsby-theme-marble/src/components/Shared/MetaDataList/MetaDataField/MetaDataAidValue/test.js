904c63bd72aed2a2247ecb06f0fc30e33419ef8b
jmille79 <38439231+jmille79@users.noreply.github.com>
2021-05-18 13:42:30 -0400

    Link finding aid to new tab

---
+++ @ndlib/gatsby-theme-marble/src/components/Shared/MetaDataList/MetaDataField/MetaDataAidValue/test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import MetaDataAidValue from './'
+
+describe('MetaDataAidValue', () => {
+  test('values', () => {
+    const values = ['value 1', 'value 2']
+    const wrapper = shallow(<MetaDataAidValue values={values} />)
+    expect(wrapper.find('dd').length).toEqual(2)
+  })
+  test('null', () => {
+    const values = []
+    const wrapper = shallow(<MetaDataAidValue values={values} />)
+    expect(wrapper.find('dd').exists()).toBeFalsy()
+  })
+})
