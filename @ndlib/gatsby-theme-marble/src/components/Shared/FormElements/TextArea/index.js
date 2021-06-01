951eac61a01216ca50953f6600d797031556f153
Paul Weikel <pweikel@nd.edu>
2021-06-01 16:27:46 -0400

    modified textarea to include text requirement

--- @ndlib/gatsby-theme-marble/src/components/Shared/FormElements/TextArea/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/FormElements/TextArea/index.js
@@ -3,7 +3,7 @@ import { jsx } from 'theme-ui'
 import PropTypes from 'prop-types'
 import sx from './sx'
 
-const TextArea = ({ id, label, defaultValue = '', onChange, disabled = false }) => {
+const TextArea = ({ id, label, defaultValue = '', onChange, disabled = false, valid, warning }) => {
   return (
     <div sx={sx.wrapper}>
       <label
@@ -12,12 +12,15 @@ const TextArea = ({ id, label, defaultValue = '', onChange, disabled = false }) 
       >{label}</label>
       <textarea
         id={id}
-        sx={sx.textArea}
+        sx={valid ? sx.textarea : sx.textInvalid}
         defaultValue={defaultValue}
         disabled={disabled}
         onChange={onChange}
         aria-label={label}
       />
+      {
+        valid ? null : <em sx={sx.warning}>{warning}</em>
+      }
     </div>
   )
 }
