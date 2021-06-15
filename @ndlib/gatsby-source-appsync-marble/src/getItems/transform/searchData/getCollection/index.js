4936530d58c6c04e692cd3f21645a0e8c49eb190
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2021-06-15 16:35:26 -0400

    MARBLE-1706 Index collection title of all ancestors, not just the closest parent.

--- @ndlib/gatsby-source-appsync-marble/src/getItems/transform/searchData/getCollection/index.js
+++ @ndlib/gatsby-source-appsync-marble/src/getItems/transform/searchData/getCollection/index.js
@@ -2,8 +2,19 @@ module.exports = (item) => {
   if (item.collections && item.collections.length > 0) {
     return item.collections.map((c) => c.display)
   } else if (item.parent) {
-    return [item.parent.title]
+    return getAncestorTitlesRecursive(item.parent)
   }
 
   return []
+}
+
+const getAncestorTitlesRecursive = (item) => {
+  let results = []
+  if (item.level === 'collection' && item.title) {
+    results.push(item.title)
+  }
+  if (item.parent) {
+    results = results.concat(getAncestorTitlesRecursive(item.parent))
+  }
+  return results
 }
