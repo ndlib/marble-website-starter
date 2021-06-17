c45631b1f17543f2620ebd30406a4e27635fe220
Jon Hartzler <jon.hartzler@gmail.com>
2021-06-17 09:12:25 -0400

    update breadcrumbs to have trimed titles. and only show the direct parent on small screens.

--- @ndlib/gatsby-theme-marble/src/components/Shared/NDBrand/Breadcrumbs/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/NDBrand/Breadcrumbs/index.js
@@ -5,6 +5,27 @@ import PropTypes from 'prop-types'
 import Link from 'components/Shared/Link'
 import { jsx } from 'theme-ui'
 
+/*
+  Class to add a ND theme breadcrumbs to a page.
+  props:
+  variant - default the style variant for changing propeties in the theme-ui. currently theme only supports default
+  breadcrumbs   - array of objects in the format of [ {url, title} ... ].
+  currentPageTitle   - the page title to put at the end
+  Breadcrumbs:
+     can have several parameters.
+      -url a link for the a tag.
+      -title for the link.
+     Do not include home or the current page.
+     Add currentPageTitle for the page title
+  Trimmed titles:
+    Titles > 60 chars get trimed on several characters see trimTitle
+    If they are still > 60 chars they get truncated
+  Theme UI Variant.
+    writes a variant called links.${variant}
+    Can be edited in the theme.
+  Examples of existing variants
+    default:
+*/
 export const NDBrandBreadcrumbs = ({ variant, breadcrumbs, currentPageTitle, ...props }) => {
   const sx = {
     pl: 0,
@@ -13,19 +34,35 @@ export const NDBrandBreadcrumbs = ({ variant, breadcrumbs, currentPageTitle, ...
     listStyle: 'none',
     fontSize: 0,
     '& li': {
-      display: 'inline-block',
+      display: ['none', 'none', 'none', 'inline-block'],
       mr: '0.75rem',
+      '&:nth-last-child(2)': {
+        display: 'inline-block',
+      },
     },
+
   }
   return (
     <ol varaint={'breadcrumbs.' + variant} sx={sx} {...props}>
       <li><Link variant='breadcrumb' to='/'>Home</Link> › </li>
       {breadcrumbs.map((item) => {
-        return (<li key={item.url}><Link variant='breadcrumb' to={item.url}>{item.title}</Link>›</li>)
+        return (<li key={item.url}><Link variant='breadcrumb' to={item.url}>{trimTitle(item.title)}</Link>›</li>)
       })}
-      <li>{currentPageTitle} › </li>
+      <li>{trimTitle(currentPageTitle)} › </li>
     </ol>
   )
+}
+
+const trimTitle = (title) => {
+  if (title.length > 60) {
+    const splitTitle = title.split(/[:_([]/)
+    if (splitTitle[0].length > 60) {
+      // match the first 60 characters up to a space or end of line
+      return splitTitle[0].match(/.{1,60}(\s|$)/g)[0].trim() + '...'
+    }
+    return splitTitle[0].trim()
+  }
+  return title
 }
 
 NDBrandBreadcrumbs.propTypes = {
