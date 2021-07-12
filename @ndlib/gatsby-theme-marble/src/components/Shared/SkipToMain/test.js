07dea3b7727db6ab33a14ef7502fe1bc2fef3bfe
Jon Hartzler <jon.hartzler@gmail.com>
2021-07-12 14:40:12 -0400

    tests

--- @ndlib/gatsby-theme-marble/src/components/Shared/SkipToMain/test.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/SkipToMain/test.js
@@ -5,11 +5,7 @@ import SkipToMain from './'
 
 test('SkipToMain', () => {
   const tree = renderer
-    .create(<SkipToMain location={{ location: { pathname: 'path' } }} />)
+    .create(<SkipToMain location={{ pathname: 'path' }} />)
     .toJSON()
   expect(tree).toMatchSnapshot()
-
-  expect(tree.props.href).toEqual('path?scrollto=maniContent')
-  expect(tree.props.className).toEqual('css-gb7gys-SkipToMain')
-  expect(tree.children[0]).toEqual('Skip to main content.')
 })
