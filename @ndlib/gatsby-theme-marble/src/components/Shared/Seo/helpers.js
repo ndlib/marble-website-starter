9c0fdba54a11d3468030334eb02b66770b645e48
Paul Weikel <pweikel@nd.edu>
2021-08-09 16:21:18 -0400

    updated return value

--- @ndlib/gatsby-theme-marble/src/components/Shared/Seo/helpers.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/Seo/helpers.js
@@ -27,7 +27,7 @@ export const getFieldValue = (field, label, data) => {
   const metaField = typy(data, 'marbleItem.metadata').safeArray.find(md => {
     return md.label === label
   })
-  return field || typy(metaField, 'value[0]').safeString || ''
+  return field || typy(metaField, 'value[0]').safeString
 }
 
 export const getAuthor = (author, data, siteMetadata) => {
