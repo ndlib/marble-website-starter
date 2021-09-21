b382baf8bb3f78e097e0099f1252749290332cc9
Jon Hartzler <jon.hartzler@gmail.com>
2021-09-21 15:57:53 -0400

    update theme file for button display

--- @ndlib/gatsby-theme-marble/src/assets/theme.js
+++ @ndlib/gatsby-theme-marble/src/assets/theme.js
@@ -213,7 +213,7 @@ const theme = merge(bootstrapTheme, {
       bg: 'white',
       '&:hover:not(:disabled)': {
         ...buttonShared['&:hover:not(:disabled)'],
-        bg: 'lightLight',
+        bg: 'white',
       },
       '& a': {
         ...buttonShared['& a'],
