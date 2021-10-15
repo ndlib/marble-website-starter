0bf37c8bff6d493d615b5862332678ab45333e88
Jon Hartzler <jon.hartzler@gmail.com>
2021-10-15 09:18:25 -0400

    fix accidental s

--- @ndlib/gatsby-source-appsync-marble-inquisition/gatsby-node.js
+++ @ndlib/gatsby-source-appsync-marble-inquisition/gatsby-node.js
@@ -2,7 +2,6 @@ const getItems = require('./src/getItems')
 const macDNS = require('./src/macDNS')
 const getItemList = require('./src/getItemList')
 const cacheRebuild = require('./src/cacheRebuild')
-const fixtureData = require('./data/fixtureData')
 const writeDebug = require('./src/writeDebug')
 
 // eslint-disable-next-line complexity
@@ -18,7 +17,7 @@ exports.sourceNodes = async (
   if (!url || !key) {
     console.error('Missing AppSync connection parameters.')
     return 1
-  } else if (!website && !itemList) {s
+  } else if (!website && !itemList) {
     console.error('Missing website or itemList.')
     return 1
   } else {
