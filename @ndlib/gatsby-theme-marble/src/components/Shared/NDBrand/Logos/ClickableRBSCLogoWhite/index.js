decc6343856d568c3888441b1493f1727c877a0d
dan <dwolfe2@nd.edu>
2021-10-21 13:46:45 -0400

    Update index.js

--- @ndlib/gatsby-theme-marble/src/components/Shared/NDBrand/Logos/ClickableRBSCLogoWhite/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/NDBrand/Logos/ClickableRBSCLogoWhite/index.js
@@ -4,7 +4,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { jsx, Image } from 'theme-ui'
 import ndLogo from '@ndlib/gatsby-theme-marble/src/assets/logos/hesburgh_mark_H2_white.svg'
-import rbsc from '@ndlib/gatsby-theme-marble/src/assets/logos/rbsc.svg'
 
 const ClickableRBSCLogoWhite = ({ variant }) => {
   return (
@@ -16,22 +15,8 @@ const ClickableRBSCLogoWhite = ({ variant }) => {
           sx={{
             height: ['60px', '45px', '45px', '45px'],
             marginBottom: '0.5rem !important',
-            width: ['400px', '400px', '400px', '255px'],
+            width: ['400px', '400px', '400px', '100%'],
           }} />
-      </a>
-      <a href='https://rarebooks.library.nd.edu/' >
-        <Image
-          src={rbsc}
-          width={30}
-          height={30}
-          alt='Rare Books & Special Collections, Hesburgh Libraries, University of Notre Dame'
-          sx={{
-            display: 'block',
-            height: ['30px'],
-            maxWidth:['30px'],
-            width: ['45px', '30px', '30px', '30px'],
-          }}
-        />
       </a>
     </div>
   )
