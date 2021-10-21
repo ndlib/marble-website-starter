d9892bb24ad70898319d4e54c678ab8eb08fe162
dan <dwolfe2@nd.edu>
2021-10-21 13:35:14 -0400

    MARBLE-1985 Fix missing inquisition translations

--- @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/index.js
+++ @ndlib/gatsby-theme-marble/src/components/Shared/ActionButtonGroup/DownloadButton/DownloadModalContent/DownloadCitation/index.js
@@ -6,15 +6,17 @@ import sx from './sx'
 
 const DownloadCitation = ({ marbleItem }) => {
   const { t } = useTranslation()
-  return marbleItem.citation ? (
-    <div sx={sx.metadata}>
-      <BaseStyles>
-        <h2 sx={sx.header}>{t('text:actionGroup.citationHead')}</h2>
-      </BaseStyles>
-      {t('text:actionGroup.citationBody')}
-      <div className='citation' sx={sx.citation}>{marbleItem.citation}</div>
-    </div>
-  ) : null
+  return marbleItem.citation
+    ? (
+      <div sx={sx.metadata}>
+        <BaseStyles>
+          <h2 sx={sx.header}>{t('text:actionGroup.citationHead')}</h2>
+        </BaseStyles>
+        {t('text:actionGroup.citationBody')}
+        <div className='citation' sx={sx.citation}>{marbleItem.citation}</div>
+      </div>
+    )
+    : null
 }
 
 export default DownloadCitation
