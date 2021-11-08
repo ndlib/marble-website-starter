07cfae3d618b59bbcf9af9b53ca7e0b45a479514
Jon Hartzler <jon.hartzler@gmail.com>
2021-11-08 10:04:28 -0500

    update to fix additional problems with locations.

--- @ndlib/gatsby-source-appsync-marble-inquisition/src/getItems/transform/searchData/getGeographicLocations/index.js
+++ @ndlib/gatsby-source-appsync-marble-inquisition/src/getItems/transform/searchData/getGeographicLocations/index.js
@@ -1,13 +1,15 @@
 module.exports = (item) => {
   if (item && item.geographicLocations && Array.isArray(item.geographicLocations)) {
     return item.geographicLocations.map((c) => {
-      const display = c.display.split(',')
-      // capture the aleph case could also use a test from item.
-      if (display.length === 2) {
-        return `${display[1]} (${display[0]})`
+      if (!c.display.match(/[(].+[,].+[)]/)) {
+        const display = c.display.split(',')
+        // capture the aleph case could also use a test from item.
+        if (display.length === 2) {
+          return `${display[1].trim()} (${display[0].trim()})`
+        }
       }
 
-      return c.display
+      return c.display.trim()
     })
   }
 
