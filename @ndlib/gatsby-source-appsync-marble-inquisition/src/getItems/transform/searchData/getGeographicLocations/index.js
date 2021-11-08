3534f3a7d91c7c4995980cc6e669cd52b5d2d9e0
Jon Hartzler <jon.hartzler@gmail.com>
2021-11-08 08:27:22 -0500

    fix the aleph location display

--- @ndlib/gatsby-source-appsync-marble-inquisition/src/getItems/transform/searchData/getGeographicLocations/index.js
+++ @ndlib/gatsby-source-appsync-marble-inquisition/src/getItems/transform/searchData/getGeographicLocations/index.js
@@ -1,6 +1,15 @@
 module.exports = (item) => {
   if (item && item.geographicLocations && Array.isArray(item.geographicLocations)) {
-    return item.geographicLocations.map((c) => c.display)
+    return item.geographicLocations.map((c) => {
+      const display = c.display.split(',')
+      // capture the aleph case could also use a test from item.
+      if (display.length === 2) {
+        console.log(`${display[1]} (${display[0]})`)
+        return `${display[1]} (${display[0]})`
+      }
+
+      return c.display
+    })
   }
 
   return []
