b0975b847e681592303af9e43a56888205f27ab3
dan <dwolfe2@nd.edu>
2022-01-18 11:01:19 -0500

    Update environment variables

--- scripts/setup-development.sh
+++ scripts/setup-development.sh
@@ -13,11 +13,11 @@ EXAMPLE_FILE="${SITE_DIR}/.env.development.example"
 
 cp ${EXAMPLE_FILE} ${ENV_FILE}
 
-GRAPHQL_API_KEY_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
+GRAPHQL_API_KEY_KEY_PATH_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
 #export FMP_CRED_PATH="/all/filemaker_credentials"
 
 # add the app sync keys to the env
-node ./scripts/environment-configuration/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> ${ENV_FILE} --unhandled-rejections=strict
+node ./scripts/environment-configuration/setupEnv.js ${GRAPHQL_API_KEY_KEY_PATH_BASE_PATH} >> ${ENV_FILE} --unhandled-rejections=strict
 
 yarn workspace $SITE install
 
