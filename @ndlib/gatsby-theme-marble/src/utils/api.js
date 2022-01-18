a59fa00e6138a63999d30a61ebfc62ceeb356338
dan <danthewolfe@users.noreply.github.com>
2022-01-18 11:59:24 -0500

    Update environment variables (#659)
    * Update environment variables
    
    * submodules

--- @ndlib/gatsby-theme-marble/src/utils/api.js
+++ @ndlib/gatsby-theme-marble/src/utils/api.js
@@ -209,7 +209,7 @@ export const removeCollectionItem = ({ item, loginReducer }) => {
 }
 
 export const getData = ({ loginReducer, contentType, query, usePublicUrl, signal }) => {
-  let url = process.env.GRAPHQL_API_URL
+  let url = process.env.GRAPHQL_API_URL_KEY_PATH
 
   const headers = {
     'Content-Type': 'application/json',
--- scripts/environment-configuration/setupEnv.js
+++ scripts/environment-configuration/setupEnv.js
@@ -6,8 +6,8 @@ const secretsmanager = new AWS.SecretsManager()
 const appConfig = process.argv.slice(2)[0]
 
 const possibleKeys = [
-  'GRAPHQL_API_KEY',
-  'GRAPHQL_API_URL',
+  'GRAPHQL_API_KEY_KEY_PATH',
+  'GRAPHQL_API_URL_KEY_PATH',
 ]
 
 const retrieveStageParameters = async () => {
@@ -20,11 +20,11 @@ const retrieveStageParameters = async () => {
     console.error('Failed getting parameter: ' + appConfig)
     console.error(err)
   })
-  params['Parameters'].forEach(node => {
-    const paramName = node['Name']
+  params.Parameters.forEach(node => {
+    const paramName = node.Name
     const envName = paramName.substring(paramName.lastIndexOf('/') + 1, paramName.length).toUpperCase().replace(/[-]/g, '_')
     if (possibleKeys.includes(envName)) {
-      console.log(`${envName}='${node['Value'].trim()}'`)
+      console.log(`${envName}='${node.Value.trim()}'`)
     }
   })
 }
