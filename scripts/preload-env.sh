f6a628196c190e3563a4a102c68057a9839e957b
dan <danthewolfe@users.noreply.github.com>
2022-01-18 14:35:52 -0500

    Revert "Update environment variables (#659)" (#660)
    This reverts commit a59fa00e6138a63999d30a61ebfc62ceeb356338.

--- scripts/preload-env.sh
+++ scripts/preload-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-GRAPHQL_API_KEY_KEY_PATH_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
+GRAPHQL_API_KEY_BASE_PATH='/all/stacks/sm-test-maintain-metadata/'
 STATIC_SITE_PATH="/all/static-host/marble-website-test/"
 FMP_CRED_PATH="/all/filemaker_credentials"
 
@@ -12,7 +12,7 @@ popd
 node ./scripts/environment-configuration/setupEnv.js ${STATIC_SITE_PATH} > './ssm-params.txt' --unhandled-rejections=strict
 
 # add the app sync keys to the env
-node ./scripts/environment-configuration/setupEnv.js ${GRAPHQL_API_KEY_KEY_PATH_BASE_PATH} >> './ssm-params.txt' --unhandled-rejections=strict
+node ./scripts/environment-configuration/setupEnv.js ${GRAPHQL_API_KEY_BASE_PATH} >> './ssm-params.txt' --unhandled-rejections=strict
 source ./ssm-params.txt
 
 rm ./ssm-params.txt
